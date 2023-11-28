70116ebdc83177a3b72efc0757e83c6c79e0937c
mbunico <mark.unico@aclcbutuan.edu.ph>
2023-11-28 09:33:22 +0800

    Update default.vue

--- layouts/default.vue
+++ layouts/default.vue
@@ -858,7 +858,7 @@
                   </li>
                   <li>
                     <NuxtLink
-                      to=".hrms/attendance/rejecteddata"
+                      to="/hrms/attendance/rejecteddata"
                       class="flex gap-x-2 items-center p-2 pl-11 w-full text-base font-normal text-gray-900 rounded-lg transition duration-75 group hover:bg-gray-100 dark:text-white dark:hover:bg-gray-700"
                     >
                       <Icon
