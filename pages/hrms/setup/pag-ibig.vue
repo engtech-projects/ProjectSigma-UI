3a4de8d24ff26020b52205d83e74cf3b3d619022
Methuselah Daniel Doysabas <124086485+DanielDoysabas@users.noreply.github.com>
2023-11-28 10:15:37 +0800

    update pag-ibig, philhealth, sss

--- pages/hrms/setup/pag-ibig.vue
+++ pages/hrms/setup/pag-ibig.vue
@@ -84,7 +84,7 @@ const submitEdit = () => {
 
   </EasyDataTable>
 
-  <form class="mt-5 edit-item w-full max-w-full bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700 p-6 overflow-auto">
+  <div class="mt-5 edit-item w-full max-w-full bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700 p-6 overflow-auto">
       <div class="mb-6">
           <label for="employee" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Employee</label>
           <input v-model="editingItem.employee" type="text" id="employee" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="" required>
@@ -94,6 +94,6 @@ const submitEdit = () => {
           <input v-model="editingItem.employee_share" type="text" id="employee_share" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="" required>
       </div> 
       <button @click="submitEdit" type="submit" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Submit</button>
-  </form>
+  </div>
 
 </template>
