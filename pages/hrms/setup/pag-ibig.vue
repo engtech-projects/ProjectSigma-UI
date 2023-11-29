9a421259db22dfb3d7e5a4ccae2d36ae96bcf2c2
Methuselah Daniel Doysabas <124086485+DanielDoysabas@users.noreply.github.com>
2023-11-29 10:40:48 +0800

    Update pagibigvue

--- pages/hrms/setup/pag-ibig.vue
+++ pages/hrms/setup/pag-ibig.vue
@@ -38,17 +38,23 @@ const editingItem = reactive({
   id: 0,
 });
 
+const addItem = reactive({
+  employee: "",
+  employee_share: "",
+  id: 0,
+});
+
 const deleteItem = (val: Item) => {
   items.value.splice(val.id,1);      
 };
 
 const editItem = (val: Item) => {
   const { employee, employee_share, id } = val;
-
   editingItem.employee = employee;
   editingItem.employee_share = employee_share;
   editingItem.id = id;
 };
+
 
 const submitEdit = () => {
   const item = items.value.find((item: { id: number; }) => item.id === editingItem.id);
@@ -56,11 +62,35 @@ const submitEdit = () => {
   item.employee_share = editingItem.employee_share;
 };
 
+const submitAdd = () => {
+};
+
+const clear = () => {
+  editingItem.employee = "";
+  editingItem.employee_share = "";
+  editingItem.id = 0;
+};
+
 </script>
 
 <template>
 
 <h2 class="text-4xl font-extrabold dark:text-white">PAG-IBIG</h2>
+
+<div class="mt-5 edit-item w-full max-w-full bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700 p-6 overflow-auto">
+    <h2 class="text-2xl font-extrabold dark:text-white">Add Pag-ibig Share</h2>
+    <div class="mt-5 b-6">
+          <label for="employee" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Employee</label>
+          <input v-model="addItem.employee" type="text" id="employee" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="" required>
+      </div> 
+      <div class="mt-5 mb-6">
+          <label for="employee_share" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Employee Share</label>
+          <input v-model="addItem.employee_share" type="text" id="employee_share" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="" required>
+      </div> 
+      <button @click="submitAdd" type="submit" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+        Add
+      </button>
+</div>
 
 <EasyDataTable
     show-index
@@ -85,15 +115,25 @@ const submitEdit = () => {
   </EasyDataTable>
 
   <div class="mt-5 edit-item w-full max-w-full bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700 p-6 overflow-auto">
+      
       <div class="mb-6">
           <label for="employee" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Employee</label>
-          <input v-model="editingItem.employee" type="text" id="employee" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="" required>
+          <input disabled readonly v-model="editingItem.employee" type="text" id="employee" class="cursor-not-allowed bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="" required>
       </div> 
+
       <div class="mb-6">
           <label for="employee_share" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Employee Share</label>
-          <input v-model="editingItem.employee_share" type="text" id="employee_share" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="" required>
+          <input aria-label="disabled input" v-model="editingItem.employee_share" type="text" id="employee_share" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="" required>
       </div> 
-      <button @click="submitEdit" type="submit" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Submit</button>
+      
+      <div class="flex flexrow gap-4">
+        <button @click="submitEdit" type="submit" class="text-white bg-green-700 hover:bg-green-800 focus:ring-4 focus:outline-none focus:ring-green-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-green-800">
+          Update
+        </button>
+        <button @click="clear" type="submit" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+          Cancel
+        </button>
+      </div>
   </div>
 
 </template>
