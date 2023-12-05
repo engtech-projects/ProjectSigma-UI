5000c61d2f6dd83a4a53602bbf1baec8ac1ea654
Methuselah Daniel Doysabas <124086485+DanielDoysabas@users.noreply.github.com>
2023-12-05 13:04:59 +0800

    Update form.vue

--- components/hrms/cashadvance/form.vue
+++ components/hrms/cashadvance/form.vue
@@ -37,12 +37,6 @@
           <textarea id="position" rows="4" class="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder=""></textarea>
         </div>
     </div>
-    <div class="mt-5">
-      <div>
-        <label for="position" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Terms of Cash Advance</label>
-        <input type="text" id="position" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" required>
-      </div>
-    </div>
     <div class="mt-5 grid grid-cols-1 gap-6 sm:grid-cols-2">
         <div>
           <label for="repayment_sched" class="block mb-2 text-md font-bold text-gray-900 dark:text-white">Repayment Schedule</label>
