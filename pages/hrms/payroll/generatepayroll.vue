5514f5a5e82e84e86c5deae87616a76a7abcb9f0
Methuselah Daniel Doysabas <124086485+DanielDoysabas@users.noreply.github.com>
2023-12-06 10:40:50 +0800

    update

--- pages/hrms/payroll/generatepayroll.vue
+++ pages/hrms/payroll/generatepayroll.vue
@@ -1,6 +1,8 @@
 <template>
-        <HrmsPayrollTable />
-        <HrmsPayrollGeneratepayrollForm />
+        <div class="mt-5 edit-item w-full max-w-full bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700 p-6 overflow-auto" >
+                <HrmsPayrollGeneratepayrollTable/>
+                <HrmsPayrollGeneratepayrollForm />
+        </div>
 </template>
 
 <script  setup lang="ts">
