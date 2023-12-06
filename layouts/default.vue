d92a817a36dbb1d7d9a0fd4bb852529f5724d8aa
Methuselah Daniel Doysabas <124086485+DanielDoysabas@users.noreply.github.com>
2023-12-06 15:35:26 +0800

    update

--- layouts/default.vue
+++ layouts/default.vue
@@ -1239,6 +1239,32 @@
               </li>
               <li>
                 <NuxtLink
+                  to="/hrms/payroll/13thmonthpay"
+                  class="flex gap-x-2 items-center p-2 pl-11 w-full text-base font-medium text-gray-900 rounded-lg transition duration-75 group hover:bg-gray-100 dark:text-white dark:hover:bg-gray-700"
+                >
+                  <Icon
+                    name="material-symbols:dynamic-form-outline-rounded"
+                    class="w-4 h-4"
+                    fill="currentColor"
+                  />
+                  13th Month Pay
+                </NuxtLink>
+              </li>
+              <li>
+                <NuxtLink
+                  to="/hrms/payroll/allowance"
+                  class="flex gap-x-2 items-center p-2 pl-11 w-full text-base font-medium text-gray-900 rounded-lg transition duration-75 group hover:bg-gray-100 dark:text-white dark:hover:bg-gray-700"
+                >
+                  <Icon
+                    name="material-symbols:dynamic-form-outline-rounded"
+                    class="w-4 h-4"
+                    fill="currentColor"
+                  />
+                  Allowance
+                </NuxtLink>
+              </li>
+              <li>
+                <NuxtLink
                   to="/hrms/payroll/payrollrecord"
                   class="flex gap-x-2 items-center p-2 pl-11 w-full text-base font-medium text-gray-900 rounded-lg transition duration-75 group hover:bg-gray-100 dark:text-white dark:hover:bg-gray-700"
                 >
