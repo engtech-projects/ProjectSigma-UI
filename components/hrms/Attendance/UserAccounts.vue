94a57f22bbffb51c7b083e65a665a1186c67f119
mbunico <mark.unico@aclcbutuan.edu.ph>
2023-12-06 14:40:43 +0800

    Update UserAccounts.vue

--- components/hrms/Attendance/UserAccounts.vue
+++ components/hrms/Attendance/UserAccounts.vue
@@ -24,7 +24,7 @@ definePageMeta({
             <SearchBar />
             <div class="border-b w-full h-[14px] text-center p-3 mt-2 mb-7">
               <span class="text-md bg-white text-black px-3 italic">
-                Login to your account
+                Setup Attendance User Login
               </span>
             </div>
 
@@ -35,7 +35,7 @@ definePageMeta({
             >
               <div>
                 <label
-                  for="email"
+                  for="username"
                   class="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
                   >Username</label
                 >
@@ -44,7 +44,7 @@ definePageMeta({
                   name="username"
                   id="username"
                   class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-                  placeholder="name@company.com"
+                  placeholder="johndoe123"
                   required
                 />
               </div>
