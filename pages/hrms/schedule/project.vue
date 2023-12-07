f480ffaee1a839a28d5950cea5fdd43366ed6505
mbunico <mark.unico@aclcbutuan.edu.ph>
2023-12-07 15:38:25 +0800

    Update project.vue

--- pages/hrms/schedule/project.vue
+++ pages/hrms/schedule/project.vue
@@ -1,4 +1,12 @@
 <script setup>
+import { onMounted } from 'vue'
+import { initFlowbite, initTabs } from 'flowbite'
+
+onMounted(() => {
+    initFlowbite();
+    initTabs();
+})
+
 
 definePageMeta({
   layout: "default",
