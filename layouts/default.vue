9ad058e17e0e9b759dd306cba3df965228ad9167
mbunico <mark.unico@aclcbutuan.edu.ph>
2023-12-07 09:22:50 +0800

    update department sched

--- layouts/default.vue
+++ layouts/default.vue
@@ -1075,7 +1075,7 @@
             </ul>
           </li>
 
-          <li>
+          <li hidden>
             <NuxtLink
               to="/project"
               class="flex items-center p-2 text-base font-medium text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group"
@@ -1252,8 +1252,59 @@
               </li>
             </ul>
           </li>
+          <li>
+            <button
+              type="button"
+              class="flex items-center p-2 w-full text-base font-medium text-gray-900 rounded-lg transition duration-75 group hover:bg-gray-100 dark:text-white dark:hover:bg-gray-700"
+              data-collapse-toggle="dropdown-schedule"
+            >
+              <Icon
+                name="uim:schedule"
+                class="flex-shrink-0 w-6 h-6 text-gray-500 transition duration-75 group-hover:text-gray-900 dark:text-gray-400 dark:group-hover:text-white"
+                fill="currentColor"
+              />
+              <span class="flex-1 ml-3 text-left whitespace-nowrap">
+                Schedule
+              </span>
+              <Icon
+                name="mdi:chevron-down"
+                class="w-6 h-6"
+                fill="currentColor"
+              />
+            </button>
+            <ul id="dropdown-schedule" class="hidden py-2 space-y-2">
+              <li>
+                <NuxtLink
+                  to="/hrms/schedule/department"
+                  class="flex gap-x-2 items-center p-2 pl-11 w-full text-base font-medium text-gray-900 rounded-lg transition duration-75 group hover:bg-gray-100 dark:text-white dark:hover:bg-gray-700"
+                >
+                  <Icon
+                    name="material-symbols:dynamic-form-outline-rounded"
+                    class="w-4 h-4"
+                    fill="currentColor"
+                  />
+                  Department Schedule
+                </NuxtLink>
+              </li>
+              <li>
+                <NuxtLink
+                  to="/hrms/schedule/employee"
+                  class="flex gap-x-2 items-center p-2 pl-11 w-full text-base font-medium text-gray-900 rounded-lg transition duration-75 group hover:bg-gray-100 dark:text-white dark:hover:bg-gray-700"
+                >
+                  <Icon
+                    name="material-symbols:dynamic-form-outline-rounded"
+                    class="w-4 h-4"
+                    fill="currentColor"
+                  />
+                  Employee Schedule
+                </NuxtLink>
+              </li>
+              
+            </ul>
+          </li>
         </ul>
       </div>
+      
       <div class="hidden absolute bottom-0 left-0 justify-center p-4 space-x-4 w-full lg:flex bg-white dark:bg-gray-800 z-20">
         <a
           href="#"
