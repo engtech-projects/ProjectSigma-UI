0729c84a65929a1703017c4301f8a61d12205801
Methuselah Daniel Doysabas <124086485+DanielDoysabas@users.noreply.github.com>
2023-12-07 11:49:47 +0800

    Update default.vue

--- layouts/default.vue
+++ layouts/default.vue
@@ -975,7 +975,7 @@
                         class="flex-shrink-0 w-6 h-6 text-gray-500 transition duration-75 group-hover:text-gray-900 dark:text-gray-400 dark:group-hover:text-white"
                         fill="currentColor"
                       />
-                      <span class="ml-3">Calendar</span>
+                      <span class="ml-3">Event Calendar</span>
                     </NuxtLink>
                 </li>
                 <li>
