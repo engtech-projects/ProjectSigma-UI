76016064e8e56b6eb4e3f5ea45d9e4532c73622f
mbunico <mark.unico@aclcbutuan.edu.ph>
2023-12-07 09:54:31 +0800

    update employee sched

---
+++ pages/hrms/schedule/employee.vue
@@ -0,0 +1,21 @@
+<script setup>
+
+definePageMeta({
+  layout: "default",
+}),
+  useHead({
+    title: "Employee Schedule",
+    meta: [{ name: "description", content: "" }],
+  });
+</script>
+
+<template>
+  <div class="text-2xl font-normal mb-4 mt-10 md:mt-0 ">
+    Employee Schedule
+  </div>
+    <hr>
+
+  <div>
+    <HrmsScheduleEmployee />
+  </div>
+</template>
