ab94fcba8f41fc15fbe0e9c78decc03bc166e7b0
mbunico <mark.unico@aclcbutuan.edu.ph>
2023-12-15 13:48:47 +0800

    absences

---
+++ components/hrms/Dashboard/Absences/index.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="w-full max-w-full bg-white border border-gray-200 rounded-lg shadow-md sm:p-6 md:p-2 dark:bg-gray-800 dark:border-gray-700">
+      <div class="flex flex-col justify-evenly gap-2">
+          <h5 class="text-xl font-medium text-gray-900 dark:text-white border-b">
+              Absences This Month
+          </h5>
+          <div class="flex flex-col gap-2">
+              <label class="" for="">Date Filter:</label>
+              <VueDatePicker v-model="date" range class="z-15" ></VueDatePicker>
+          </div>
+      </div>
+      <div class="mt-5 overflow-auto min-h-96 max-h-96">
+          <div class="grid grid-cols-4 justify-start mt-4 gap-4 p-2">
+            <div v-for="(employee, index) in employees" :key="i0ndex" class="flex flex-col items-center">
+              <HrmsDashboardAbsencesItem 
+                title="Generate Memo" :name="employee.name" :avatar="employee.avatar" :absences="employee.absences"> 
+              </HrmsDashboardAbsencesItem>
+            </div>
+          </div>
+      </div>
+  </div>
+</template>
+
+<script>
+import VueDatePicker from '@vuepic/vue-datepicker';
+export default {
+  props: {
+      employees: Array, // An array of employee objects with properties: name, avatar, birthdate
+  },
+  methods: {
+      formatDate(date) {
+          const options = { month: 'long', day: 'numeric', year: 'numeric' };
+          return new Date(date).toLocaleDateString(undefined, options);
+      },
+  },
+};
+</script>
--- pages/hrms/index.vue
+++ pages/hrms/index.vue
@@ -1,37 +1,83 @@
 <template>
-    <div class="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-2 gap-4 mb-4">
-      <div
-        class="grid-cols-2 border-gray-300 rounded-lg dark:border-gray-600 h-full md:h-full"
-      >
-        <HrmsDashboardAnnouncement class="mt-10 md:mt-0 p-2 " />
-      </div>
-      <div
-        class="border-gray-300 rounded-lg dark:border-gray-600 h-full md:h-full"
-      >
-        <HrmsDashboardBirthdays :employees="employeesData" class="md: mt-2 md:mt-0 p-2" />
-      </div>
-      <div class="border-gray-300 rounded-lg dark:border-gray-600 h-full md:h-full">
-        <HrmsDashboardLate :employees="employeeslateData" class="md: mt-2 md:mt-0 p-2" />
-      </div>
-      <div
-        class=" rounded-lg border-gray-300 dark:border-gray-600 h-92 md:h-full"
-      >
-        <HrmsDashboardEmployeeStatus/>
-      </div>
-      <div
-        class=" rounded-lg border-gray-300 dark:border-gray-600 h-full md:h-full"
-      >
-        <ChartEmployeeschart title="Employee Lates and Absences Chart" :labels="['Lates','Absences']" :datas="['22','40']" class="p-2"/>
-      </div>
-      <div
-        class=" rounded-lg border-gray-300 dark:border-gray-600 h-full md:h-full"
-      >
-        <ChartEmployeeschart title="Employee Chart" :labels="['Accounting', 'Finance', 'HR', 'Legal', 'Engineer', 'Estimator', 'Equipment Operator', 'Marketing', 'Sales', 'IT', 'Field Staff', 'Operations']" :datas="['40', '20', '12', '14', '56', '45', '88', '65', '56', '49', '71', '92']" class="p-2"/>
-      </div>
-      
+  <div class="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-2 gap-4 mb-4">
+    <div
+      class="grid-cols-2 border-gray-300 rounded-lg dark:border-gray-600 h-full md:h-full"
+    >
+      <HrmsDashboardAnnouncementTimeline class="mt-10 md:mt-0 p-2" />
     </div>
+    <div
+      class="border-gray-300 rounded-lg dark:border-gray-600 h-full md:h-full"
+    >
+      <HrmsDashboardBirthdays
+        :employees="employeesData"
+        class="md: mt-2 md:mt-0 p-2"
+      />
+    </div>
+    <div
+      class="border-gray-300 rounded-lg dark:border-gray-600 h-full md:h-full"
+    >
+      <HrmsDashboardLates
+        :employees="employeeslateData"
+        class="md: mt-2 md:mt-0 p-2"
+      />
+    </div>
+    <div
+      class="border-gray-300 rounded-lg dark:border-gray-600 h-full md:h-full"
+    >
+      <HrmsDashboardAbsences
+        :employees="employeesabsencesData"
+        class="md: mt-2 md:mt-0 p-2"
+      />
+    </div>
+    <div
+      class="rounded-lg border-gray-300 dark:border-gray-600 h-full md:h-full"
+    >
+      <ChartEmployeeschart
+        title="Employee Lates and Absences Chart"
+        :labels="['Lates', 'Absences']"
+        :datas="['22', '40']"
+        class="p-2"
+      />
+    </div>
+    <div
+      class="rounded-lg border-gray-300 dark:border-gray-600 h-full md:h-full"
+    >
+      <ChartEmployeeschart
+        title="Employee Chart"
+        :labels="[
+          'Accounting',
+          'Finance',
+          'HR',
+          'Legal',
+          'Engineer',
+          'Estimator',
+          'Equipment Operator',
+          'Marketing',
+          'Sales',
+          'IT',
+          'Field Staff',
+          'Operations',
+        ]"
+        :datas="[
+          '40',
+          '20',
+          '12',
+          '14',
+          '56',
+          '45',
+          '88',
+          '65',
+          '56',
+          '49',
+          '71',
+          '92',
+        ]"
+        class="p-2"
+      />
+    </div>
+  </div>
 
-    <!-- <div
+  <!-- <div
       class="border-2 border-dashed rounded-lg border-gray-300 dark:border-gray-600 h-96 mb-4"
     >
       5
@@ -105,6 +151,14 @@ const employeeslateData = ref([
   { name: "Mark Unico", avatar: "/avatarexample.png", late: "2" },
 ]);
 
+const employeesabsencesData = ref([
+  { name: "John Mayer", avatar: "/avatarexample.png", absences: "2" },
+  { name: "Jane Smith", avatar: "/avatarexample.png", absences: "3" },
+  { name: "John Doe", avatar: "/avatarexample.png", absences: "3" },
+  { name: "Mark Unico", avatar: "/avatarexample.png", absences: "3" },
+  { name: "John Doe", avatar: "/avatarexample.png", absences: "3" },
+  { name: "Mark Unico", avatar: "/avatarexample.png", absences: "2" },
+]);
 // initialize components based on data attribute selectors
 onMounted(() => {
   initFlowbite();
