68561b68cff8b8dccad94eea7055574a8b5adb0e
Daniel Doysabas <124086485+DanielDoysabas@users.noreply.github.com>
2023-12-31 12:10:13 +0800

    Update nav.vue

--- components/layout/header/nav.vue
+++ components/layout/header/nav.vue
@@ -187,7 +187,7 @@
             </div>
         
             <!-- Profile dropdown-menu -->
-            <div class="hidden z-50 my-4 w-56 text-base list-none bg-white rounded divide-y divide-gray-100 shadow dark:bg-gray-700 dark:divide-gray-600 rounded-xl" id="dropdown">
+            <div class="hidden z-50 my-4 w-56 text-base list-none bg-white rounded divide-y divide-gray-100 shadow dark:bg-gray-700 dark:divide-gray-600 rounded-xl" id="user-menu-button">
                 <div class="py-3 px-4">
                     <span
                     class="block text-sm font-semibold text-gray-900 dark:text-white"
