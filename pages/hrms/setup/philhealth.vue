8cd63ef2b3d58e576c96132191118dcf29335177
JoseJanlofreDy <jose@mybizniche.com>
2024-01-15 19:26:53 +0800

    eslint no multiple template root

--- pages/hrms/setup/philhealth.vue
+++ pages/hrms/setup/philhealth.vue
@@ -78,115 +78,114 @@ const submitAdd = () => {
 </script>
 
 <template>
-    <h2 class="text-2xl font-semibold dark:text-white">
-        PHILHEALTH
-    </h2>
-
-    <div class="mt-5 edit-item w-full max-w-full bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700 p-6 overflow-auto">
-        <label for="" class="text-xl font-semibold text-gray-900">Add Contribution Record</label>
-
-        <div class="mt-5 grid gap-6 mb-6 md:grid-cols-2">
-            <div>
-                <label for="add_salary_val_start" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Start</label>
+    <div>
+        <h2 class="text-2xl font-semibold dark:text-white">
+            PHILHEALTH
+        </h2>
+        <div class="mt-5 edit-item w-full max-w-full bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700 p-6 overflow-auto">
+            <label for="" class="text-xl font-semibold text-gray-900">Add Contribution Record</label>
+    
+            <div class="mt-5 grid gap-6 mb-6 md:grid-cols-2">
+                <div>
+                    <label for="add_salary_val_start" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Start</label>
+                    <input
+                        id="add_salary_val_start"
+                        v-model="addItem.salary_val_start"
+                        type="text"
+                        class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                        placeholder=""
+                        required
+                    >
+                </div>
+                <div>
+                    <label for="add_salary_val_end" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">End</label>
+                    <input
+                        id="add_salary_val_end"
+                        v-model="addItem.salary_val_end"
+                        type="text"
+                        class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                        placeholder=""
+                        required
+                    >
+                </div>
+            </div>
+            <div class="mb-6">
+                <label for="add_monthly_prem" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Monthly Premium</label>
                 <input
-                    id="add_salary_val_start"
-                    v-model="addItem.salary_val_start"
+                    id="add_monthly_prem"
+                    v-model="addItem.monthly_prem"
                     type="text"
                     class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
                     placeholder=""
                     required
                 >
             </div>
-            <div>
-                <label for="add_salary_val_end" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">End</label>
-                <input
-                    id="add_salary_val_end"
-                    v-model="addItem.salary_val_end"
-                    type="text"
-                    class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-                    placeholder=""
-                    required
-                >
-            </div>
+            <button type="submit" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800" @click="submitAdd">
+                Add
+            </button>
         </div>
-        <div class="mb-6">
-            <label for="add_monthly_prem" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Monthly Premium</label>
-            <input
-                id="add_monthly_prem"
-                v-model="addItem.monthly_prem"
-                type="text"
-                class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-                placeholder=""
-                required
+        <div class="mt-5 edit-item w-full max-w-full bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700 p-6 overflow-auto">
+            <label for="" class="text-xl font-semibold text-gray-900">Contribution Table</label>
+            <EasyDataTable
+                show-index
+                :headers="headers"
+                :items="items"
+                class="mt-5"
             >
+                <template #item-actions="item">
+                    <button
+                        @click="deleteItem(item)"
+                    >
+                        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path fill="currentColor" d="M19 4h-3.5l-1-1h-5l-1 1H5v2h14M6 19a2 2 0 0 0 2 2h8a2 2 0 0 0 2-2V7H6z" /></svg>
+                    </button>
+                    <button
+                        @click="editItem(item)"
+                    >
+                        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path fill="currentColor" d="M20.71 7.04c.39-.39.39-1.04 0-1.41l-2.34-2.34c-.37-.39-1.02-.39-1.41 0l-1.84 1.83l3.75 3.75M3 17.25V21h3.75L17.81 9.93l-3.75-3.75z" /></svg>
+                    </button>
+                </template>
+            </EasyDataTable>
         </div>
-        <button type="submit" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800" @click="submitAdd">
-            Add
-        </button>
-    </div>
-
-    <div class="mt-5 edit-item w-full max-w-full bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700 p-6 overflow-auto">
-        <label for="" class="text-xl font-semibold text-gray-900">Contribution Table</label>
-        <EasyDataTable
-            show-index
-            :headers="headers"
-            :items="items"
-            class="mt-5"
-        >
-            <template #item-actions="item">
-                <button
-                    @click="deleteItem(item)"
-                >
-                    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path fill="currentColor" d="M19 4h-3.5l-1-1h-5l-1 1H5v2h14M6 19a2 2 0 0 0 2 2h8a2 2 0 0 0 2-2V7H6z" /></svg>
-                </button>
-                <button
-                    @click="editItem(item)"
-                >
-                    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path fill="currentColor" d="M20.71 7.04c.39-.39.39-1.04 0-1.41l-2.34-2.34c-.37-.39-1.02-.39-1.41 0l-1.84 1.83l3.75 3.75M3 17.25V21h3.75L17.81 9.93l-3.75-3.75z" /></svg>
-                </button>
-            </template>
-        </EasyDataTable>
-    </div>
-
-    <div class="mt-5 edit-item w-full max-w-full bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700 p-6 overflow-auto">
-        <label for="" class="text-xl font-semibold text-gray-900">Edit Contribution Record</label>
-        <div class="mt-5 grid gap-6 mb-6 md:grid-cols-2">
-            <div>
-                <label for="salary_val_start" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Start</label>
+        <div class="mt-5 edit-item w-full max-w-full bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700 p-6 overflow-auto">
+            <label for="" class="text-xl font-semibold text-gray-900">Edit Contribution Record</label>
+            <div class="mt-5 grid gap-6 mb-6 md:grid-cols-2">
+                <div>
+                    <label for="salary_val_start" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Start</label>
+                    <input
+                        id="salary_val_start"
+                        v-model="editingItem.salary_val_start"
+                        type="text"
+                        class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                        placeholder=""
+                        required
+                    >
+                </div>
+                <div>
+                    <label for="salary_val_end" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">End</label>
+                    <input
+                        id="salary_val_end"
+                        v-model="editingItem.salary_val_end"
+                        type="text"
+                        class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                        placeholder=""
+                        required
+                    >
+                </div>
+            </div>
+            <div class="mb-6">
+                <label for="monthly_prem" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Monthly Premium</label>
                 <input
-                    id="salary_val_start"
-                    v-model="editingItem.salary_val_start"
+                    id="monthly_prem"
+                    v-model="editingItem.monthly_prem"
                     type="text"
                     class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
                     placeholder=""
                     required
                 >
             </div>
-            <div>
-                <label for="salary_val_end" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">End</label>
-                <input
-                    id="salary_val_end"
-                    v-model="editingItem.salary_val_end"
-                    type="text"
-                    class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-                    placeholder=""
-                    required
-                >
-            </div>
+            <button type="submit" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800" @click="submitEdit">
+                Submit
+            </button>
         </div>
-        <div class="mb-6">
-            <label for="monthly_prem" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Monthly Premium</label>
-            <input
-                id="monthly_prem"
-                v-model="editingItem.monthly_prem"
-                type="text"
-                class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-                placeholder=""
-                required
-            >
-        </div>
-        <button type="submit" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800" @click="submitEdit">
-            Submit
-        </button>
     </div>
 </template>
