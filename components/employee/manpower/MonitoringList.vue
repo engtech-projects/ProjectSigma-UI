522611f767d3aa6d2a7050d63eca058a2bc0c48b
mbunico <mark.unico@aclcbutuan.edu.ph>
2024-02-07 13:24:28 +0800

    Update MonitoringList.vue

--- components/employee/manpower/MonitoringList.vue
+++ components/employee/manpower/MonitoringList.vue
@@ -69,52 +69,5 @@ const boardLoading = ref(false)
         >
             {{ successMessage }}
         </p>
-        <!-- <div
-            class="shadow-md p-4 mt-5 bg-white mb-3 border border-gray-200 rounded-lg w-full"
-        >
-            <EasyTable
-                show-index
-                :headers="headers"
-                :items="items"
-                class="mt-5"
-            >
-                <template #item-actions="item">
-                    <button
-                        @click="approveItem(item)"
-                    >
-                        <Icon name="material-symbols:check-circle" color="green" class="w-5 h-5" />
-                    </button>
-                    <button
-                        @click="denyItem(item)"
-                    >
-                        <Icon name="material-symbols:cancel" color="red" class="w-5 h-5" />
-                    </button>
-                    <button
-                        @click="viewItem(item)"
-                    >
-                        <Icon name="material-symbols:visibility-rounded" color="blue" class="w-5 h-5" />
-                    </button>
-                </template>
-            </EasyTable>
-
-            <div v-if="selectedItemDetails">
-                <div class="fixed inset-0 bg-black opacity-50 " />
-                <div class="fixed inset-0 flex items-center justify-center ">
-                    <div class="bg-white p-8 max-w-lg rounded-lg border border-slate-300">
-                        <h2 class="text-lg font-semibold mb-4">
-                            Item Details
-                        </h2>
-                        <ul>
-                            <li><strong>Employee Name:</strong> {{ selectedItemDetails.employee_name }}</li>
-                            <li><strong>Department:</strong> {{ selectedItemDetails.department_name }}</li>
-                            <li><strong>Action Taken:</strong> {{ selectedItemDetails.action_taken }}</li>
-                        </ul>
-                        <button class="mt-4" @click="closeViewModal">
-                            Close
-                        </button>
-                    </div>
-                </div>
-            </div>
-        </div> -->
     </LayoutBoards>
 </template>
