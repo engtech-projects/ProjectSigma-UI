14b7011c9d30dc622c2b1bf2368914709ddaf1db
Rustom Pedales <pedales.rustom@gmail.com>
2024-03-18 14:34:00 +0800

    Create ListView.vue

---
+++ components/layout/Approvals/ListView.vue
@@ -0,0 +1,28 @@
+<script setup>
+defineProps({
+    approvals: {
+        type: Object,
+        required: true,
+    },
+})
+</script>
+<template>
+    <div class="p-2 w-full">
+        <p class="text-blue-700 text-center">
+            Approvals
+        </p>
+        <div v-for="(apprv, index) in approvals" :key="index" class="bg-white w-full">
+            <ol class="flex w-full justify-between">
+                <li class="w-full p-2 text-left">
+                    {{ apprv.type }}
+                </li>
+                <li class="w-full p-2 text-left">
+                    {{ apprv.fullname }}
+                </li>
+                <li class="w-full p-2 text-center">
+                    {{ apprv.status }}
+                </li>
+            </ol>
+        </div>
+    </div>
+</template>
