3fcb08451f980aab15fb0b66fb0c22eb7431282c
Rustom Pedales <pedales.rustom@gmail.com>
2024-03-18 14:35:49 +0800

    Update PersonalActionFormInfo.vue

--- components/hrms/Employee/Pan/PersonalActionFormInfo.vue
+++ components/hrms/Employee/Pan/PersonalActionFormInfo.vue
@@ -62,20 +62,7 @@ defineProps({
                         {{ panData.work_location }}
                     </dd>
                 </div>
-                <div>
-                    <p class="text-blue-700 text-center">
-                        Approvals
-                    </p>
-                    {{ panData.approvals }}
-                    <!-- <div v-for="apprv, index in panData.approvals" :key="index" class="bg-white px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
-                        <dt class="text-sm font-medium text-gray-500">
-                            {{ apprv.user_id }}
-                        </dt>
-                        <dd class="mt-1 text-sm text-gray-900 sm:mt-0 sm:col-span-2">
-                            {{ apprv.status }}
-                        </dd>
-                    </div> -->
-                </div>
+                <LayoutApprovalsListView :approvals="JSON.parse(panData.approvals)" />
             </dl>
         </div>
     </div>
