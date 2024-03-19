74debea5e93fee8dcc6dabcb27857f3347d823cf
Rustom Pedales <pedales.rustom@gmail.com>
2024-03-19 10:25:53 +0800

    requested_by

--- components/layout/Approvals/ListView.vue
+++ components/layout/Approvals/ListView.vue
@@ -13,15 +13,28 @@ defineProps({
         </p>
         <div v-for="(apprv, index) in approvals" :key="index" class="bg-white w-full">
             <ol class="flex w-full justify-between">
-                <li class="w-full p-2 text-left">
-                    {{ apprv.type }}
-                </li>
-                <li class="w-full p-2 text-left">
-                    {{ apprv.name }}
-                </li>
-                <li class="w-full p-2 text-center">
-                    {{ apprv.status }}
-                </li>
+                <template v-if="apprv.employee">
+                    <li class="w-full p-2 text-left">
+                        {{ apprv.type }}
+                    </li>
+                    <li class="w-full p-2 text-left">
+                        {{ apprv.employee.full_name }}
+                    </li>
+                    <li class="w-full p-2 text-center">
+                        {{ apprv.status }}
+                    </li>
+                </template>
+                <template v-else>
+                    <li class="w-full p-2 text-left">
+                        {{ apprv.type }}
+                    </li>
+                    <li class="w-full p-2 text-left">
+                        {{ apprv.name }}
+                    </li>
+                    <li class="w-full p-2 text-center">
+                        {{ apprv.status }}
+                    </li>
+                </template>
             </ol>
         </div>
     </div>
