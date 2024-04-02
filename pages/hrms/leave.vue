8dbf0ed6b6af7bfa94f96adfa84866a158403213
Rustom Pedales <pedales.rustom@gmail.com>
2024-04-02 15:01:21 +0800

    implement leaveRequest in form

--- pages/hrms/leave.vue
+++ pages/hrms/leave.vue
@@ -1,17 +1,27 @@
-<script setup lang="ts">
+<script setup>
+import { useLeaveRequest } from "@/stores/hrms/leaveRequest"
 
 useHead({
     title: "Leave",
     meta: [
         { name: "description", content: "" }
     ],
-
 })
+const leaveRequest = useLeaveRequest()
+leaveRequest.allLeaves()
+leaveRequest.allApprovals()
+leaveRequest.allRequest()
 </script>
 
 <template>
-    <div>
-        <HrmsLeaveForm />
-        <HrmsLeaveTable />
+    <div class="w-full flex p-2">
+        <div class="md:w-2/3 p-2 w-full">
+            <HrmsLeaveForm />
+        </div>
+        <div class="md:w-1/3 p-2 w-full">
+            <HrmsLeaveAllLeaveList />
+            <HrmsLeaveApprovalList />
+            <HrmsLeaveMyLeaveList />
+        </div>
     </div>
 </template>
