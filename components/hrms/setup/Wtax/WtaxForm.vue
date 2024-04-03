3f53afe0e0c7e390c2f293739ea2237d336bdd52
Jose Janlofre Dy <35553424+janlofredy@users.noreply.github.com>
2024-04-03 01:00:19 +0000

    move setup wtax stores

--- components/hrms/setup/Wtax/WtaxForm.vue
+++ components/hrms/setup/Wtax/WtaxForm.vue
@@ -1,6 +1,6 @@
 <script setup>
 import { storeToRefs } from "pinia"
-import { useWtaxStore, TERM } from "@/stores/hrms/wtax"
+import { useWtaxStore, TERM } from "@/stores/hrms/setup/wtax"
 
 const withholdings = useWtaxStore()
 const { withholding, errorMessage, successMessage } = storeToRefs(withholdings)
