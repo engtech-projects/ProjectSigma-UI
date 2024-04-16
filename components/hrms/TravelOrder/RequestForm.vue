6218badc924fd4bec7d253fce8c1189fabb34614
janlofredy <janlofredy@gmail.com>
2024-04-16 02:58:57 +0000

    fix requested by field

--- components/hrms/TravelOrder/RequestForm.vue
+++ components/hrms/TravelOrder/RequestForm.vue
@@ -91,7 +91,7 @@ const submitForm = async () => {
                             <label for="requestedBy" class="text-sm italic font-semibold text-gray-700">Requested By</label>
                             <input
                                 :value="userData.name"
-                                type="number"
+                                type="text"
                                 disabled
                                 class="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
                             >
