65f29571404b56038edb5e573cf08e8bcec2b77f
mbunico <mark.unico@aclcbutuan.edu.ph>
2024-04-16 15:41:52 +0800

    Update MultipleEmployeeSelector.vue

--- components/hrms/Common/MultipleEmployeeSelector.vue
+++ components/hrms/Common/MultipleEmployeeSelector.vue
@@ -10,7 +10,7 @@ const model = defineModel({ required: true, type: Array<Number> })
             v-model="allEmployeeEnum.params.filterType"
             class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
         >
-            <option value="">
+            <option value="" selected>
                 Select Filter Type
             </option>
             <option value="Project">
@@ -34,12 +34,17 @@ const model = defineModel({ required: true, type: Array<Number> })
 
         <table>
             <tr>
-                <th>Employee Name</th>
+                <th class="italic pb-2 pt-2">
+                    Employee Name
+                </th>
             </tr>
             <tr v-for="employee, vfkey in enums.filteredEmployeesList" :key="vfkey">
-                <td>
-                    <input v-model="model" type="checkbox" name="employeeSelectorCheckBoxes" :value="employee.id">
-                    <label :for="'employeeSelectorCheckBox'+vfkey">{{ employee.fullname_last }}</label>
+                <td class="space-x-2 px-px">
+                    <label class="flex items-center space-x-2">
+                        <input v-model="model" type="checkbox" name="employeeSelectorCheckBoxes" :value="employee.id" class="h-5 w-5 cursor-pointer appearance-none rounded-md border border-blue-gray-200 transition-all checked:border-gray-900 checked:bg-gray-900 hover:before:opacity-10">
+                        <span class="cursor-pointer text-sm">{{ employee.fullname_last }}</span>
+                    </label>
+                    <hr class="h-px my-1 bg-gray-200 border-0 dark:bg-gray-700">
                 </td>
             </tr>
         </table>
