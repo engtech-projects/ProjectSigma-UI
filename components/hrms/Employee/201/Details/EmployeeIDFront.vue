bde2d9a91a6deadcd4288c633bfce2e2bd9732d4
Rustom Pedales <pedales.rustom@gmail.com>
2024-04-17 16:27:17 +0800

    Update EmployeeIDFront.vue

--- components/hrms/Employee/201/Details/EmployeeIDFront.vue
+++ components/hrms/Employee/201/Details/EmployeeIDFront.vue
@@ -8,17 +8,17 @@ const { information } = storeToRefs(employee)
 <template>
     <div id="front-id" class="w-full shadow-lg relative bg-white border-none">
         <img class="w-full min-w-80 cover" src="/front.fw.png" alt="front" draggable="false">
-        <div class="w-full absolute md:top-[9rem] top-[10rem] text-center">
+        <div class="w-full absolute md:top-[24.5%] top-[25%] text-center">
             <p class="font-light text-xl">
                 {{ information.company_employments.employeedisplay_id }}
             </p>
         </div>
-        <div class="w-full absolute md:top-[27rem] top-[30.5rem] text-center">
+        <div class="w-full absolute md:top-[70.5%] top-[70.5%] text-center">
             <p class="text-xl font-bold">
                 {{ information.fullname_first }}
             </p>
         </div>
-        <div class="w-full absolute md:top-[29.5rem] top-[32.5rem] text-center">
+        <div class="w-full absolute md:top-[75.5%] top-[75.5%] text-center">
             <p class="text-xl font-bold">
                 DIVISION SUPERVISOR
             </p>
