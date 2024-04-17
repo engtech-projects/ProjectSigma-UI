c5631111465f513678b484dd666337130b6dbb98
mbunico <mark.unico@aclcbutuan.edu.ph>
2024-04-17 11:21:33 +0800

    overtime update

--- components/hrms/overtime/OvertimeFormReq.vue
+++ components/hrms/overtime/OvertimeFormReq.vue
@@ -1,33 +1,28 @@
 <script setup lang="ts">
 import { storeToRefs } from "pinia"
-import type { Item, Header } from "vue3-easy-data-table"
-import { useEnumsStore } from "@/stores/hrms/enum"
+// import type { Item, Header } from "vue3-easy-data-table"
+// import { useEnumsStore } from "@/stores/hrms/enum"
 import { useOvertimeStore } from "@/stores/hrms/overtime"
-import { useProjectStore } from "@/stores/project-monitoring/projects"
 import { useApprovalStore, APPROVAL_OVERTIME } from "@/stores/hrms/setup/approvals"
 
-const enums = useEnumsStore()
-const { allEmployeeEnum } = storeToRefs(enums)
+// const enums = useEnumsStore()
+// const { allEmployeeEnum } = storeToRefs(enums)
 
 const approvals = useApprovalStore()
 
 const overtimes = useOvertimeStore()
 const { overtime, errorMessage, successMessage } = storeToRefs(overtimes)
 
-const projects = useProjectStore()
-const { list: projectList } = storeToRefs(projects)
-
 overtime.value.approvals = await approvals.getApprovalByName(APPROVAL_OVERTIME)
 
 const snackbar = useSnackbar()
 const boardLoading = ref(false)
-const selectedEmployees = ref<Item[]>([])
 
-const headers: Header[] = [
-    {
-        text: "Employee Name", value: "fullname_last",
-    },
-]
+// const headers: Header[] = [
+//     {
+//         text: "Employee Name", value: "fullname_last",
+//     },
+// ]
 
 const formatTime = (time: string) => {
     const timeParts = time.split(":")
@@ -48,7 +43,6 @@ const submitForm = async () => {
         boardLoading.value = true
         overtime.value.overtime_start_time = formatTime(overtime.value.overtime_start_time)
         overtime.value.overtime_end_time = formatTime(overtime.value.overtime_end_time)
-        overtime.value.employees = selectedEmployees.value.map(emp => emp.id)
         await overtimes.createRequest()
         if (overtimes.errorMessage !== "") {
             snackbar.add({
@@ -81,23 +75,17 @@ const submitForm = async () => {
                     <div class="flex-1 gap-4">
                         <div class="pt-2">
                             <label for="project_name" class="block mb-1 text-sm font-medium text-gray-900 dark:text-white">Project Selection</label>
-                            <select id="project_name" v-model="overtime.project_id" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
-                                <option value="" disabled selected>
-                                    Choose Project
-                                </option>
-                                <option v-for="proj, id in projectList" :key="id" :value="proj.id">
-                                    {{ proj.contract_name }}
-                                </option>
-                            </select>
+                            <HrmsCommonProjectSelector v-model="overtime.project_id" />
                         </div>
                         <label for="selectEmp" class="block mt-2 text-sm font-medium text-gray-900 dark:text-white">Employee Selection</label>
-                        <EasyDataTable
+                        <HrmsCommonMultipleEmployeeSelector v-model="overtime.employees" title="Employee Name" name="Employee Name" />
+                        <!-- <EasyDataTable
                             v-model:items-selected="selectedEmployees"
                             :rows-per-page="10"
                             class="mt-5"
                             :headers="headers"
                             :items="allEmployeeEnum.list"
-                        />
+                        /> -->
                     </div>
                     <div class="flex-1 flex-col gap-4 p-2">
                         <div>
