5542715e82ca98c68a7e3dc95a21a6bf95d76577
Rustom Pedales <pedales.rustom@gmail.com>
2024-04-29 08:18:23 +0800

    create facialPattern Store

---
+++ stores/hrms/facialPattern.ts
@@ -0,0 +1,55 @@
+import { defineStore } from "pinia"
+
+export const useFacialPattern = defineStore("facialPattern", {
+    state: () => ({
+        facialPatterList: [],
+        currentMatch: { id: null, distance: null },
+        errorMessage: "",
+        successMessage: "",
+    }),
+    actions: {
+        async getAllEmployeePattern () {
+            this.successMessage = ""
+            this.errorMessage = ""
+            await useHRMSApiO(
+                "/api/face-pattern/resource",
+                {
+                    method: "GET",
+                    onResponse: ({ response }: any) => {
+                        if (response.ok) {
+                            this.facialPatterList = response._data
+                        } else {
+                            this.errorMessage = response._data.message
+                            throw new Error(response._data.message)
+                        }
+                    },
+                }
+            )
+        },
+
+        async saveOrUpdateEmployeePattern (pattern: any, id: any) {
+            this.successMessage = ""
+            this.errorMessage = ""
+            const formData = new FormData()
+            formData.append("employee_id", id)
+            formData.append("patterns", JSON.stringify(pattern))
+            await useHRMSApiO(
+                "/api/face-pattern/resource",
+                {
+                    method: "POST",
+                    body: formData,
+                    onResponse: ({ response }: any) => {
+                        if (response.ok) {
+                            this.successMessage = response._data.message
+                            return response._data
+                        } else {
+                            this.errorMessage = response._data.message
+                            throw new Error(response._data.message)
+                        }
+                    },
+                }
+            )
+        },
+
+    },
+})
