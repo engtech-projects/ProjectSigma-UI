64731bdc22959f0c8710368b6956646c1955d41c
Rustom Pedales <pedales.rustom@gmail.com>
2024-05-06 10:31:55 +0800

    Create qr-code.vue

---
+++ pages/attendance-portal/qr-code.vue
@@ -0,0 +1,14 @@
+<script setup>
+definePageMeta({
+    layout: "guest",
+    auth: {
+        unauthenticatedOnly: true,
+        navigateAuthenticatedTo: "/dashboard",
+    },
+})
+</script>
+<template>
+    <div>
+        QR CODE
+    </div>
+</template>
