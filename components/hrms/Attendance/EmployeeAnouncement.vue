9b4413b1a6566f604f12755fba11772066bb611f
Rustom Pedales <pedales.rustom@gmail.com>
2024-05-09 15:18:23 +0800

    add attendance announcement

---
+++ components/hrms/Attendance/EmployeeAnouncement.vue
@@ -0,0 +1,19 @@
+<script setup>
+import { useAnnouncements } from "@/stores/hrms/announcements"
+const mains = useAnnouncements()
+mains.getactiveAll()
+
+useHead({
+    title: "Dashboard",
+    meta: [{ name: "description", content: "SIGMA Dashboard" }],
+    bodyAttrs: {
+        class: "test",
+    },
+})
+</script>
+
+<template>
+    <div class="w-full">
+        <HrmsDashboardAnnouncementTimeline />
+    </div>
+</template>
