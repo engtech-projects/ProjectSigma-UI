2d72693829d745e6b6a1151938c3a9ff5ee16fb9
Rustom Pedales <pedales.rustom@gmail.com>
2024-05-13 14:35:01 +0800

    add footer template

---
+++ components/AttendancePortal/FacialRecognition/employee-attendance-footer.vue
@@ -0,0 +1,23 @@
+<template>
+    <div class="flex footer w-full bg-gray-200 text-gray-700 h-12 justify-between px-4">
+        <div class="flex flex-col gap-2 justify-center">
+            <p>Powered By: Engtech Global Solutions Inc.</p>
+        </div>
+        <div class="flex flex-col gap-2 justify-center">
+            <p>HDS Building 999 JC. Aquino St. Butuan City</p>
+        </div>
+        <div class="flex flex-col gap-2 justify-center">
+            <p>Mobile No: +63 928 559 5074</p>
+        </div>
+        <div class="flex flex-col gap-2 justify-center">
+            <p>FB Page: <a href="https://www.facebook.com/engtechofficial"><span class="text-blue-700">Engtech Global Solutions Inc</span></a> </p>
+        </div>
+    </div>
+</template>
+<style scoped>
+    .footer {
+        position: fixed;
+        left: 0;
+        bottom: 0;
+    }
+</style>
