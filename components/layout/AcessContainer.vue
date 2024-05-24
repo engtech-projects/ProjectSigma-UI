33ae16799d379121f923b05aa5d0f6f9860dd915
Rustom Pedales <pedales.rustom@gmail.com>
2024-05-24 12:14:23 +0800

    add access container

---
+++ components/layout/AcessContainer.vue
@@ -0,0 +1,19 @@
+<script setup>
+defineProps({
+    ifAccess: {
+        type: Boolean,
+        require: true,
+    }
+})
+</script>
+<template>
+    <div
+        v-if="ifAccess"
+        class="w-full"
+    >
+        <slot />
+    </div>
+    <div v-else>
+        <UnauthorizeAccess />
+    </div>
+</template>
