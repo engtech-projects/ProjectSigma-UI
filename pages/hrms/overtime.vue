c4629b6f29fbe7c71995acefde3dca2929aeb23d
Jose Janlofre Dy <35553424+janlofredy@users.noreply.github.com>
2024-06-05 09:28:29 +0800

    overtime Accessibilities

--- pages/hrms/overtime.vue
+++ pages/hrms/overtime.vue
@@ -22,14 +22,19 @@ useHead({
 <template>
     <LayoutAcessContainer
         :if-access="useCheckAccessibility([
-            AccessibilityTypes.hrms_lnotnto_travel_order_form,
+            AccessibilityTypes.hrms_lnotnto_overtime_group,
         ])"
     >
         <div
             class="grid grid-cols-2 md:grid-cols-2 gap-4"
         >
             <div>
-                <HrmsOvertimeFormReq v-show="!isEdit" />
+                <HrmsOvertimeFormReq
+                    v-if="useCheckAccessibility([
+                        AccessibilityTypes.hrms_lnotnto_overtime_form,
+                    ])"
+                    v-show="!isEdit"
+                />
                 <HrmsEditOvertime v-show="isEdit" />
             </div>
             <div>
