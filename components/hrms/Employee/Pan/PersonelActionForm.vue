a75a128d543590953966b6073f06f0940e46a460
Rustom Pedales <pedales.rustom@gmail.com>
2024-06-10 14:53:54 +0800

    remove redundunt title and new search bar for new hire

--- components/hrms/Employee/Pan/PersonelActionForm.vue
+++ components/hrms/Employee/Pan/PersonelActionForm.vue
@@ -28,6 +28,9 @@ const savePan = async () => {
         boardLoading.value = false
     }
 }
+const selectApplicant = (data) => {
+    console.log(data)
+}
 </script>
 <template>
     <LayoutBoards
@@ -98,23 +101,13 @@ const savePan = async () => {
                             </td>
                         </tr>
                         <tr>
-                            <td colspan="4">
-                                <HrmsEmployeePanSearchBar />
-                            </td>
-                        </tr>
-                        <tr>
                             <td colspan="2" class="border border-slate-300 p-2">
                                 <div>
                                     <label
                                         for="small-input"
                                         class="block mb-2 text-[11px] font-medium text-gray-900 dark:text-white"
-                                    >EMPLOYEE:</label>
-                                    <input
-                                        id="small-input"
-                                        v-model="pan.personelActionNotice.employee"
-                                        type="text"
-                                        class="block w-full p-2 text-gray-900 border border-gray-300 rounded-md bg-gray-50 sm:text-xs focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-                                    >
+                                    >APPLICANT:</label>
+                                    <HrmsEmployeePanSearchBar @search-changed="selectApplicant" />
                                 </div>
                             </td>
                             <td class="border border-slate-300 p-2">
