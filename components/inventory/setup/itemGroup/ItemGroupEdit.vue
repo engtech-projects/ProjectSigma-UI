604ab8ca5c43c4fa0fcd7d43e3a20e807b176101
Daniel Doysabas <124086485+DanielDoysabas@users.noreply.github.com>
2024-08-03 11:51:45 +0800

    Update ItemGroupEdit.vue

--- components/inventory/setup/itemGroup/ItemGroupEdit.vue
+++ components/inventory/setup/itemGroup/ItemGroupEdit.vue
@@ -39,7 +39,6 @@ const cancelUpdateItemGroup = () => {
             <div class="flex flex-row justify-center gap-1">
                 <div class="flex flex-col">
                     <input
-                        v-model="name"
                         type="text"
                         class="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none focus:outline-none focus:ring-0 focus:border-cyan-600 peer"
                     >
