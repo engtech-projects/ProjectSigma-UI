0699b1297535fd2ba7d1c95459b0fa9ee94418b4
Jose Janlofre Dy <janlofredy@gmail.com>
2024-08-16 10:46:00 +0800

    selectsearch fix not removing focus when selecting input

--- components/layout/Form/PsSelectSearch.vue
+++ components/layout/Form/PsSelectSearch.vue
@@ -13,6 +13,7 @@ const props = defineProps({
         default: [],
     }
 })
+const forFocusOut = ref()
 const result = defineModel("result", { type: Object, required: true })
 const searchInput = defineModel("searchInput", { type: String, required: true })
 const showDD = ref(false)
@@ -30,7 +31,7 @@ const closeDD = () => {
 }
 function selectOption (option: any) {
     result.value = option
-    closeDD()
+    forFocusOut.value.focus()
 }
 function clearSelection () {
     result.value = {}
@@ -38,41 +39,49 @@ function clearSelection () {
 </script>
 
 <template>
-    <div id="PSSELECTSEARCH" tabindex="50" @focusin="openDD" @focusout="closeDD">
-        <div class="border border-slate-600 rounded-md px-3 text-md flex items-center relative cursor-pointer">
-            <div class="h-full flex flex-1 items-center overflow-hidden py-[9px]">
-                <input
-                    v-if="showDD"
-                    v-model="searchInput"
-                    type="text"
-                    class="border border-slate-300 rounded w-full h-full"
-                    placeholder="Search"
-                    @click.stop
-                >
-                <span v-else class="flex-1">{{ result ? result[title] : placeholder }}</span>
-                <div v-show="result" @click="clearSelection">
-                    <Icon name="material-symbols:close-rounded" class="font-bold text-xl" />
-                </div>
-                <Icon name="iconoir:nav-arrow-down" class="font-bold text-xl" />
-            </div>
-        </div>
-        <div class="relative">
-            <div
-                v-if="showDD"
-                class="absolute max-h-72 left-0 min-w-full py-2 px-2 border border-slate-800 bg-white rounded flex flex-col gap-2"
-            >
-                <div v-if="props.searchList.length" class="flex flex-col overflow-auto">
-                    <span
-                        v-for="option, i in props.searchList"
-                        :key="i"
-                        class="cursor-pointer hover:bg-slate-100 px-3 py-1 border-b"
-                        @click="selectOption(option)"
+    <div ref="forFocusOut" tabindex="51">
+        <div
+            id="PSSELECTSEARCH"
+            ref="compContainer"
+            tabindex="50"
+            @focusin="openDD"
+            @focusout="closeDD"
+        >
+            <div class="border border-slate-600 rounded-md px-3 text-md flex items-center relative cursor-pointer">
+                <div class="h-full flex flex-1 items-center overflow-hidden py-[9px]">
+                    <input
+                        v-if="showDD"
+                        v-model="searchInput"
+                        type="text"
+                        class="border border-slate-300 rounded w-full h-full"
+                        placeholder="Search"
+                        @click.stop
                     >
-                        {{ option[props.title] }}
-                    </span>
+                    <span v-else class="flex-1">{{ result ? result[title] : placeholder }}</span>
+                    <div v-show="result" @click="clearSelection">
+                        <Icon name="material-symbols:close-rounded" class="font-bold text-xl" />
+                    </div>
+                    <Icon name="iconoir:nav-arrow-down" class="font-bold text-xl" />
                 </div>
-                <div v-else class="px-4">
-                    <span>No data available.</span>
+            </div>
+            <div class="relative">
+                <div
+                    v-if="showDD"
+                    class="absolute max-h-72 left-0 min-w-full py-2 px-2 border border-slate-800 bg-white rounded flex flex-col gap-2"
+                >
+                    <div v-if="props.searchList.length" class="flex flex-col overflow-auto">
+                        <span
+                            v-for="option, i in props.searchList"
+                            :key="i"
+                            class="cursor-pointer hover:bg-slate-100 px-3 py-1 border-b"
+                            @click="selectOption(option)"
+                        >
+                            {{ option[props.title] }}
+                        </span>
+                    </div>
+                    <div v-else class="px-4">
+                        <span>No data available.</span>
+                    </div>
                 </div>
             </div>
         </div>
