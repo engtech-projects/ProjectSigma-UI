7e29f808458452ada8ed74d5e1407d37f5fec192
Jose Janlofre Dy <janlofredy@gmail.com>
2024-08-28 15:50:51 +0800

    remove name duplicate

--- components/hrms/Attendance/failtolog/FormLog.vue
+++ components/hrms/Attendance/failtolog/FormLog.vue
@@ -26,7 +26,7 @@ const submitAdd = async () => {
                 text: failtologs.successMessage
             })
         }
-        failtolog.value.employee_id = ""
+        failtolog.value.employee_id = null
         failtolog.value.approvals = await approvals.getApprovalByName(APPROVAL_FAILTOLOG)
     } catch {
         snackbar.add({
@@ -45,10 +45,7 @@ const submitAdd = async () => {
         <form @submit.prevent="submitAdd">
             <div class="w-full p-2">
                 <div class="mt-2 grid gap-6 mb-2 md:grid-cols-2">
-                    <div>
-                        <label for="employee" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Name</label>
-                        <HrmsCommonSearchEmployeeSelector v-model="failtolog.employee_id" />
-                    </div>
+                    <HrmsCommonSearchEmployeeSelector v-model="failtolog.employee_id" />
                     <div class="w-full">
                         <label for="department" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Log Type</label>
                         <select v-model="failtolog.log_type" name="" class="border border-gray-500 rounded-md">
