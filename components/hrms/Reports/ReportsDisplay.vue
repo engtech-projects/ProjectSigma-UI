36f68774ba2a80b03d90a06f97c8b741783bf099
Rustom Pedales <pedales.rustom@gmail.com>
2024-09-06 16:55:42 +0800

    [Update]: change report select layout

--- components/hrms/Reports/ReportsDisplay.vue
+++ components/hrms/Reports/ReportsDisplay.vue
@@ -1,121 +1,12 @@
+<script setup lang="ts">
+defineProps({
+    selectedReport: {
+        type: String,
+        default: null,
+    }
+})
+</script>
 <template>
-    <div>
-        <div class="md:grid grid-cols-4 gap-4 mt-5 mb-16">
-            <select id="report" v-model="selectedReport" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
-                <option value="" disabled selected>
-                    Select Report
-                </option>
-                <option
-                    v-if="useCheckAccessibility(['AdminOnly'])"
-                    value="Report1"
-                >
-                    SSS Summary
-                </option>
-                <option
-                    v-if="useCheckAccessibility(['AdminOnly'])"
-                    value="Report2"
-                >
-                    SSS Project Remittance
-                </option>
-                <option
-                    v-if="useCheckAccessibility([AccessibilityTypes.hrms_reports_sssemployeeremittance])"
-                    value="Report3"
-                >
-                    SSS Employee Remittance
-                </option>
-                <option
-                    v-if="useCheckAccessibility(['AdminOnly'])"
-                    value="Report4"
-                >
-                    SSS Loan Payment Summary
-                </option>
-                <option
-                    v-if="useCheckAccessibility(['AdminOnly'])"
-                    value="Report5"
-                >
-                    SSS Loan Payment
-                </option>
-                <option
-                    v-if="useCheckAccessibility(['AdminOnly'])"
-                    value="Report6"
-                >
-                    PHIC Summary
-                </option>
-                <option
-                    v-if="useCheckAccessibility(['AdminOnly'])"
-                    value="Report7"
-                >
-                    PHIC Project Remittance
-                </option>
-                <option
-                    v-if="useCheckAccessibility([AccessibilityTypes.hrms_reports_philhealthemployeeremittance])"
-                    value="Report8"
-                >
-                    PHIC Remittance
-                </option>
-                <option
-                    v-if="useCheckAccessibility(['AdminOnly'])"
-                    value="Report9"
-                >
-                    HDMF Summary
-                </option>
-                <option
-                    v-if="useCheckAccessibility(['AdminOnly'])"
-                    value="Report10"
-                >
-                    HDMF Project Remittance
-                </option>
-                <option
-                    v-if="useCheckAccessibility([AccessibilityTypes.hrms_reports_pagibigemployeeremittance])"
-                    value="Report11"
-                >
-                    HDMF Remittance
-                </option>
-                <option
-                    v-if="useCheckAccessibility(['AdminOnly'])"
-                    value="Report12"
-                >
-                    HDMF Loan Summary
-                </option>
-                <option
-                    v-if="useCheckAccessibility(['AdminOnly'])"
-                    value="Report13"
-                >
-                    HDMF Loan Project Remittance
-                </option>
-                <option
-                    v-if="useCheckAccessibility(['AdminOnly'])"
-                    value="Report14"
-                >
-                    HDMF Loan Remittance
-                </option>
-                <option
-                    v-if="useCheckAccessibility(['AdminOnly'])"
-                    value="Report15"
-                >
-                    Salary Monitoring
-                </option>
-                <option
-                    v-if="useCheckAccessibility(['AdminOnly'])"
-                    value="Report16"
-                >
-                    Overtime Monitoring
-                </option>
-                <option
-                    v-if="useCheckAccessibility(['AdminOnly'])"
-                    value="Report17"
-                >
-                    Project Payroll Summary
-                </option>
-                <option
-                    v-if="useCheckAccessibility(['AdminOnly'])"
-                    value="Report18"
-                >
-                    Employee Logs
-                </option>
-            </select>
-        </div>
-    </div>
     <div id="printContent">
         <HrmsReportsSssContribution
             v-show="selectedReport==='Report1'"
@@ -175,11 +66,6 @@
         <Icon name="ic:baseline-local-printshop" class="w-4 h-4 mr-1" fill="currentColor" />Print Report
     </button>
 </template>
-
-<script setup lang="ts">
-const selectedReport = ref(null)
-</script>
-
 <style>
     #printContainer {
         display: none;
--- pages/hrms/reports.vue
+++ pages/hrms/reports.vue
@@ -1,3 +1,12 @@
+<script setup lang="ts">
+useHead({
+    title: "Reports",
+    meta: [
+        { name: "description", content: "" }
+    ],
+})
+const selectedReport = ref("")
+</script>
 <template>
     <LayoutAcessContainer
         :if-access="useCheckAccessibility([
@@ -7,23 +16,128 @@
         <div
             class="w-full"
         >
-            <div class="text-2xl font-normal mb-4 mt-0  ">
-                Reports
+            <div class="flex gap-4">
+                <div class="text-2xl font-normal mb-4 mt-0  ">
+                    Reports
+                </div>
+                <select id="report" v-model="selectedReport" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block p-2 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
+                    <option value="" disabled selected>
+                        Select Report
+                    </option>
+                    <option
+                        v-if="useCheckAccessibility(['AdminOnly'])"
+                        value="Report1"
+                    >
+                        SSS Summary
+                    </option>
+                    <option
+                        v-if="useCheckAccessibility(['AdminOnly'])"
+                        value="Report2"
+                    >
+                        SSS Project Remittance
+                    </option>
+                    <option
+                        v-if="useCheckAccessibility([AccessibilityTypes.hrms_reports_sssemployeeremittance])"
+                        value="Report3"
+                    >
+                        SSS Employee Remittance
+                    </option>
+                    <option
+                        v-if="useCheckAccessibility(['AdminOnly'])"
+                        value="Report4"
+                    >
+                        SSS Loan Payment Summary
+                    </option>
+                    <option
+                        v-if="useCheckAccessibility(['AdminOnly'])"
+                        value="Report5"
+                    >
+                        SSS Loan Payment
+                    </option>
+                    <option
+                        v-if="useCheckAccessibility(['AdminOnly'])"
+                        value="Report6"
+                    >
+                        PHIC Summary
+                    </option>
+                    <option
+                        v-if="useCheckAccessibility(['AdminOnly'])"
+                        value="Report7"
+                    >
+                        PHIC Project Remittance
+                    </option>
+                    <option
+                        v-if="useCheckAccessibility([AccessibilityTypes.hrms_reports_philhealthemployeeremittance])"
+                        value="Report8"
+                    >
+                        PHIC Remittance
+                    </option>
+                    <option
+                        v-if="useCheckAccessibility(['AdminOnly'])"
+                        value="Report9"
+                    >
+                        HDMF Summary
+                    </option>
+                    <option
+                        v-if="useCheckAccessibility(['AdminOnly'])"
+                        value="Report10"
+                    >
+                        HDMF Project Remittance
+                    </option>
+                    <option
+                        v-if="useCheckAccessibility([AccessibilityTypes.hrms_reports_pagibigemployeeremittance])"
+                        value="Report11"
+                    >
+                        HDMF Remittance
+                    </option>
+                    <option
+                        v-if="useCheckAccessibility(['AdminOnly'])"
+                        value="Report12"
+                    >
+                        HDMF Loan Summary
+                    </option>
+                    <option
+                        v-if="useCheckAccessibility(['AdminOnly'])"
+                        value="Report13"
+                    >
+                        HDMF Loan Project Remittance
+                    </option>
+                    <option
+                        v-if="useCheckAccessibility(['AdminOnly'])"
+                        value="Report14"
+                    >
+                        HDMF Loan Remittance
+                    </option>
+                    <option
+                        v-if="useCheckAccessibility(['AdminOnly'])"
+                        value="Report15"
+                    >
+                        Salary Monitoring
+                    </option>
+                    <option
+                        v-if="useCheckAccessibility(['AdminOnly'])"
+                        value="Report16"
+                    >
+                        Overtime Monitoring
+                    </option>
+                    <option
+                        v-if="useCheckAccessibility(['AdminOnly'])"
+                        value="Report17"
+                    >
+                        Project Payroll Summary
+                    </option>
+                    <option
+                        v-if="useCheckAccessibility(['AdminOnly'])"
+                        value="Report18"
+                    >
+                        Employee Logs
+                    </option>
+                </select>
             </div>
             <hr>
             <div>
-                <HrmsReportsDisplay />
+                <HrmsReportsDisplay :selected-report="selectedReport" />
             </div>
         </div>
     </LayoutAcessContainer>
 </template>
-
-<script setup lang="ts">
-useHead({
-    title: "Reports",
-    meta: [
-        { name: "description", content: "" }
-    ],
-
-})
-</script>
