cb38223b76a375dbc7145ccd41d2508cc1748296
Daniel Doysabas <124086485+DanielDoysabas@users.noreply.github.com>
2024-09-11 17:57:17 +0800

    [Update] - Item Request Export and Print

--- components/inventory/ItemProfile/ItemProfileDetails.vue
+++ components/inventory/ItemProfile/ItemProfileDetails.vue
@@ -89,128 +89,131 @@ const denyRequest = async (id:any) => {
         boardLoading.value = false
     }
 }
-
 </script>
 <template>
     <div
         class="h-full w-full bg-white border border-gray-200 rounded-lg shadow-md sm:p-6 md:p-2 dark:bg-gray-800 dark:border-gray-700"
     >
         <div class="flex flex-col gap-2 w-full p-4">
-            <div id="headline">
+            <div id="headline mb-4">
                 <div class="basis-[10%] grow-1 shrink-0 flex items-center justify-between border-b rounded-t">
                     <h3 v-if="title" class="pl-4 text-xl font-semibold text-gray-900 p-4">
                         {{ title }}
                     </h3>
                 </div>
             </div>
-            <div id="content">
-                <table class="table-auto w-full border-collapse">
-                    <thead>
-                        <tr>
-                            <InventoryCommonTableItemTh title="Item Code" />
-                            <InventoryCommonTableItemTh title="Item description" />
-                            <InventoryCommonTableItemTh title="Thickness" />
-                            <InventoryCommonTableItemTh title="Length" />
-                            <InventoryCommonTableItemTh title="Width" />
-                            <InventoryCommonTableItemTh title="Height" />
-                            <InventoryCommonTableItemTh title="Outside diameter" />
-                            <InventoryCommonTableItemTh title="Inside diameter" />
-                            <InventoryCommonTableItemTh title="Volume" />
-                            <InventoryCommonTableItemTh title="Specification" />
-                            <InventoryCommonTableItemTh title="Grade" />
-                            <InventoryCommonTableItemTh title="Color" />
-                            <InventoryCommonTableItemTh title="UOM" />
-                            <InventoryCommonTableItemTh title="Sub Item Group" />
-                            <InventoryCommonTableItemTh title="Item Group" />
-                            <InventoryCommonTableItemTh title="Inventory Type" />
-                        </tr>
-                    </thead>
-                    <tbody>
-                        <tr v-for="dataValue, index in data" :key="index" class="bg-white border-b">
-                            <td class="px-2 font-medium text-gray-900 whitespace-nowrap text-start">
-                                <div class="flex flex-row justify-center gap-2">
-                                    <template v-if="dataValue.similar_items.length >= 1">
-                                        <div class="icon">
-                                            <Icon name="material-symbols:info-outline" class="text-blue-700 h-5 w-5 lg:h-5 lg:w-5" data-tooltip-target="tooltip-top" />
-                                            <div id="tooltip-top" data-popover role="tooltip" class="absolute z-[9999] duration-300 invisible inline-block px-3 py-2 text-sm font-medium text-white bg-gray-900 rounded-lg shadow-sm opacity-0 tooltip">
-                                                <div class="list flex flex-col gap-2">
-                                                    <div class="text-md">
-                                                        Similar Items:
-                                                    </div>
-                                                    <template v-for="item in dataValue.similar_items" :key="item + '-summary'">
-                                                        <div class="flex flex-col gap-1 text-xs">
-                                                            {{ item.item_description }}
+            <LayoutPrint>
+                <div id="itemDetails">
+                    <div id="content">
+                        <table class="table-auto w-full border-collapse">
+                            <thead>
+                                <tr>
+                                    <InventoryCommonTableItemTh title="Item Code" />
+                                    <InventoryCommonTableItemTh title="Item description" />
+                                    <InventoryCommonTableItemTh title="Thickness" />
+                                    <InventoryCommonTableItemTh title="Length" />
+                                    <InventoryCommonTableItemTh title="Width" />
+                                    <InventoryCommonTableItemTh title="Height" />
+                                    <InventoryCommonTableItemTh title="Outside diameter" />
+                                    <InventoryCommonTableItemTh title="Inside diameter" />
+                                    <InventoryCommonTableItemTh title="Volume" />
+                                    <InventoryCommonTableItemTh title="Specification" />
+                                    <InventoryCommonTableItemTh title="Grade" />
+                                    <InventoryCommonTableItemTh title="Color" />
+                                    <InventoryCommonTableItemTh title="UOM" />
+                                    <InventoryCommonTableItemTh title="Sub Item Group" />
+                                    <InventoryCommonTableItemTh title="Item Group" />
+                                    <InventoryCommonTableItemTh title="Inventory Type" />
+                                </tr>
+                            </thead>
+                            <tbody>
+                                <tr v-for="dataValue, index in data" :key="index" class="bg-white border-b">
+                                    <td class="px-2 font-medium text-gray-900 whitespace-nowrap text-start">
+                                        <div class="flex flex-row justify-center gap-2">
+                                            <template v-if="dataValue.similar_items.length >= 1">
+                                                <div class="icon">
+                                                    <Icon name="material-symbols:info-outline" class="text-blue-700 h-5 w-5 lg:h-5 lg:w-5" data-tooltip-target="tooltip-top" />
+                                                    <div id="tooltip-top" data-popover role="tooltip" class="absolute z-[9999] duration-300 invisible inline-block px-3 py-2 text-sm font-medium text-white bg-gray-900 rounded-lg shadow-sm opacity-0 tooltip">
+                                                        <div class="list flex flex-col gap-2">
+                                                            <div class="text-md">
+                                                                Similar Items:
+                                                            </div>
+                                                            <template v-for="item in dataValue.similar_items" :key="item + '-summary'">
+                                                                <div class="flex flex-col gap-1 text-xs">
+                                                                    {{ item.item_description }}
+                                                                </div>
+                                                            </template>
                                                         </div>
-                                                    </template>
+                                                        <div class="tooltip-arrow" data-popper-arrow />
+                                                    </div>
                                                 </div>
-                                                <div class="tooltip-arrow" data-popper-arrow />
+                                            </template>
+                                            <div class="context">
+                                                {{ dataValue.sku }}
                                             </div>
                                         </div>
-                                    </template>
-                                    <div class="context">
-                                        {{ dataValue.sku }}
-                                    </div>
-                                </div>
-                            </td>
-                            <td class="px-2 font-medium text-gray-900 whitespace-nowrap text-start">
-                                {{ dataValue.item_description }}
-                            </td>
-                            <td class="px-2 font-medium text-gray-900 whitespace-nowrap text-start">
-                                {{ dataValue.thickness_val }}
-                                {{ getType(dataValue.thickness_uom) }}
-                            </td>
-                            <td class="px-2 font-medium text-gray-900 whitespace-nowrap text-start">
-                                {{ dataValue.length_val }}
-                                {{ getType(dataValue.length_uom) }}
-                            </td>
-                            <td class="px-2 font-medium text-gray-900 whitespace-nowrap text-start">
-                                {{ dataValue.width_val }}
-                                {{ getType(dataValue.width_uom) }}
-                            </td>
-                            <td class="px-2 font-medium text-gray-900 whitespace-nowrap text-start">
-                                {{ dataValue.height_val }}
-                                {{ getType(dataValue.height_uom) }}
-                            </td>
-                            <td class="px-2 font-medium text-gray-900 whitespace-nowrap text-start">
-                                {{ dataValue.outside_diameter_val }}
-                                {{ getType(dataValue.outside_diameter_uom) }}
-                            </td>
-                            <td class="px-2 font-medium text-gray-900 whitespace-nowrap text-start">
-                                {{ dataValue.inside_diameter_val }}
-                                {{ getType(dataValue.inside_diameter_uom) }}
-                            </td>
-                            <td class="px-2 font-medium text-gray-900 whitespace-nowrap text-start">
-                                {{ dataValue.volume }}
-                                {{ getType(dataValue.volume_uom) }}
-                            </td>
-                            <td class="px-2 font-medium text-gray-900 whitespace-nowrap text-start">
-                                {{ dataValue.specification }}
-                            </td>
-                            <td class="px-2 font-medium text-gray-900 whitespace-nowrap text-start">
-                                {{ dataValue.grade }}
-                            </td>
-                            <td class="px-2 font-medium text-gray-900 whitespace-nowrap text-start">
-                                {{ dataValue.color }}
-                            </td>
-                            <td class="px-2 font-medium text-gray-900 whitespace-nowrap text-start">
-                                {{ dataValue.uom }}
-                            </td>
-                            <td class="px-2 font-medium text-gray-900 whitespace-nowrap text-start">
-                                {{ dataValue.sub_item_group }}
-                            </td>
-                            <td class="px-2 font-medium text-gray-900 whitespace-nowrap text-start">
-                                {{ dataValue.item_group }}
-                            </td>
-                            <td class="px-2 font-medium text-gray-900 whitespace-nowrap text-start">
-                                {{ dataValue.inventory_type }}
-                            </td>
-                        </tr>
-                    </tbody>
-                </table>
-            </div>
-            <div id="approvals" class="w-full">
-                <LayoutApprovalsListView :approvals="dataApproval" />
-            </div>
+                                    </td>
+                                    <td class="px-2 font-medium text-gray-900 whitespace-nowrap text-start">
+                                        {{ dataValue.item_description }}
+                                    </td>
+                                    <td class="px-2 font-medium text-gray-900 whitespace-nowrap text-start">
+                                        {{ dataValue.thickness_val }}
+                                        {{ getType(dataValue.thickness_uom) }}
+                                    </td>
+                                    <td class="px-2 font-medium text-gray-900 whitespace-nowrap text-start">
+                                        {{ dataValue.length_val }}
+                                        {{ getType(dataValue.length_uom) }}
+                                    </td>
+                                    <td class="px-2 font-medium text-gray-900 whitespace-nowrap text-start">
+                                        {{ dataValue.width_val }}
+                                        {{ getType(dataValue.width_uom) }}
+                                    </td>
+                                    <td class="px-2 font-medium text-gray-900 whitespace-nowrap text-start">
+                                        {{ dataValue.height_val }}
+                                        {{ getType(dataValue.height_uom) }}
+                                    </td>
+                                    <td class="px-2 font-medium text-gray-900 whitespace-nowrap text-start">
+                                        {{ dataValue.outside_diameter_val }}
+                                        {{ getType(dataValue.outside_diameter_uom) }}
+                                    </td>
+                                    <td class="px-2 font-medium text-gray-900 whitespace-nowrap text-start">
+                                        {{ dataValue.inside_diameter_val }}
+                                        {{ getType(dataValue.inside_diameter_uom) }}
+                                    </td>
+                                    <td class="px-2 font-medium text-gray-900 whitespace-nowrap text-start">
+                                        {{ dataValue.volume }}
+                                        {{ getType(dataValue.volume_uom) }}
+                                    </td>
+                                    <td class="px-2 font-medium text-gray-900 whitespace-nowrap text-start">
+                                        {{ dataValue.specification }}
+                                    </td>
+                                    <td class="px-2 font-medium text-gray-900 whitespace-nowrap text-start">
+                                        {{ dataValue.grade }}
+                                    </td>
+                                    <td class="px-2 font-medium text-gray-900 whitespace-nowrap text-start">
+                                        {{ dataValue.color }}
+                                    </td>
+                                    <td class="px-2 font-medium text-gray-900 whitespace-nowrap text-start">
+                                        {{ dataValue.uom }}
+                                    </td>
+                                    <td class="px-2 font-medium text-gray-900 whitespace-nowrap text-start">
+                                        {{ dataValue.sub_item_group }}
+                                    </td>
+                                    <td class="px-2 font-medium text-gray-900 whitespace-nowrap text-start">
+                                        {{ dataValue.item_group }}
+                                    </td>
+                                    <td class="px-2 font-medium text-gray-900 whitespace-nowrap text-start">
+                                        {{ dataValue.inventory_type }}
+                                    </td>
+                                </tr>
+                            </tbody>
+                        </table>
+                    </div>
+                    <div id="approvals" class="w-full">
+                        <LayoutApprovalsListView :approvals="dataApproval" />
+                    </div>
+                </div>
+            </LayoutPrint>
             <div id="footer">
                 <div v-if="dataApproval.next_approval?.user_id === userData?.id" class="flex gap-2 p-2 justify-end relative">
                     <HrmsCommonApprovalDenyButton
