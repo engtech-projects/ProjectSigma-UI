9798a871a6062926d483af69fa22337fe5cca122
Jose Janlofre Dy <janlofredy@gmail.com>
2024-09-17 17:13:29 +0800

    fail to log fix charging id

--- components/hrms/Attendance/failtolog/FormLog.vue
+++ components/hrms/Attendance/failtolog/FormLog.vue
@@ -2,46 +2,43 @@
 import { useFailToLogStore, LOG_TYPE } from "@/stores/hrms/attendance/failtolog"
 import { useApprovalStore, APPROVAL_FAILTOLOG } from "~/stores/hrms/setup/approvals"
 
-const failtologs = useFailToLogStore()
-const { failtolog } = storeToRefs(failtologs)
+const failLogStore = useFailToLogStore()
+const { createRequestData } = storeToRefs(failLogStore)
 
 const approvals = useApprovalStore()
-failtolog.value.approvals = await approvals.getApprovalByName(APPROVAL_FAILTOLOG)
+createRequestData.value.data.approvals = await approvals.getApprovalByName(APPROVAL_FAILTOLOG)
 
 const snackbar = useSnackbar()
-const boardLoading = ref(false)
 
 const submitAdd = async () => {
     try {
-        const backupEmployeeSelected = failtolog.value.employee_id
-        boardLoading.value = true
-        await failtologs.createLog()
+        const backupEmployeeSelected = createRequestData.value.data.employee_id
+        await failLogStore.createLog()
         // failtolog.value.employee_id = null // Removed keep old employee because hard to reset lol
         snackbar.add({
             type: "success",
-            text: failtologs.successMessage
+            text: createRequestData.value.successMessage
         })
-        failtolog.value.employee_id = backupEmployeeSelected
+        createRequestData.value.data.employee_id = backupEmployeeSelected
     } catch {
         snackbar.add({
             type: "error",
-            text: failtologs.errorMessage
+            text: createRequestData.value.errorMessage ?? "Something Went Wrong"
         })
     } finally {
-        boardLoading.value = false
-        failtologs.clearMessages()
+        failLogStore.clearMessages()
     }
 }
 </script>
 <template>
-    <LayoutBoards title="Failure to Log Form" class="mt-2 edit-item w-full max-w-full" :loading="boardLoading">
+    <LayoutBoards title="Failure to Log Form" class="mt-2 edit-item w-full max-w-full" :loading="createRequestData.isLoading">
         <form @submit.prevent="submitAdd">
             <div class="w-full p-2">
                 <div class="mt-2 grid gap-2 mb-2 md:grid-cols-2">
-                    <HrmsCommonSearchEmployeeSelector v-model="failtolog.employee_id" />
+                    <HrmsCommonSearchEmployeeSelector v-model="createRequestData.data.employee_id" />
                     <div class="w-full">
                         <label for="department" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Log Type</label>
-                        <select v-model="failtolog.log_type" name="" class="border border-gray-500 rounded-md">
+                        <select v-model="createRequestData.data.log_type" name="" class="border border-gray-500 rounded-md">
                             <option value="" disabled selected>
                                 Choose Log Type
                             </option>
@@ -51,27 +48,27 @@ const submitAdd = async () => {
                         </select>
                     </div>
                     <div>
-                        <LayoutFormPsDateInput v-model="failtolog.date" title="Date" required />
+                        <LayoutFormPsDateInput v-model="createRequestData.data.date" title="Date" required />
                     </div>
                     <div>
                         <label for="time" class="block mb-1 text-sm font-medium text-gray-900 dark:text-white">Time</label>
                         <input
                             id="time"
-                            v-model="failtolog.time"
+                            v-model="createRequestData.data.time"
                             type="time"
                             class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full md:w-1/2 p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
                             required
                         >
                     </div>
                     <HrmsCommonDepartmentProjectSelector
-                        v-model:select-type="failtolog.charging_type"
-                        v-model:department-id="failtolog.charging_id"
-                        v-model:project-id="failtolog.charging_id"
+                        v-model:select-type="createRequestData.data.charging_type"
+                        v-model:department-id="createRequestData.data.department_id"
+                        v-model:project-id="createRequestData.data.project_id"
                         title="Charging"
                         class="p-2"
                     />
                     <div class="w-full">
-                        <LayoutFormPsTextInput v-model="failtolog.reason" title="Reason" />
+                        <LayoutFormPsTextInput v-model="createRequestData.data.reason" title="Reason" />
                     </div>
                 </div>
 
@@ -79,9 +76,9 @@ const submitAdd = async () => {
                     <div class="pt-5 w-full mb-2 rounded-lg p-4 bg-slate-100 ">
                         <label for="approved_by" class="block text-sm font-medium text-gray-900 dark:text-white">Recommending Approval:</label>
                         <HrmsSetupApprovalsList
-                            v-for="(approv, apr) in failtolog.approvals"
+                            v-for="(approv, apr) in createRequestData.data.approvals"
                             :key="'hrmsetupapprovallist' + apr"
-                            v-model="failtolog.approvals[apr]"
+                            v-model="createRequestData.data.approvals[apr]"
                         />
                     </div>
                     <!-- <div class="w-full">
