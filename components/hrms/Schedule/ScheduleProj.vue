a1e5f7cf8edc162d5ce660af626395f00ec43000
Jose Janlofre Dy <janlofredy@gmail.com>
2024-09-19 11:04:43 +0800

    move css to scoped for calendars

--- components/hrms/Schedule/ScheduleProj.vue
+++ components/hrms/Schedule/ScheduleProj.vue
@@ -476,3 +476,19 @@ watch(errorMessage, (msg) => {
         </div>
     </div>
 </template>
+
+<style scoped>
+/* FULL CALENDAR STYLES */
+#fCalendar {
+    :deep(.fc-button-active) {
+        z-index: 0 !important;
+    }
+    :deep(.event-container) {
+        height: 23px!important;
+        transition: all .3s;
+    }
+    :deep(.event-container:hover) {
+        height: 39px!important
+    }
+}
+</style>
