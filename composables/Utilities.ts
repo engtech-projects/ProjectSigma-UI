d451d726aced797808f0513134dd100455a438c8
Jose Janlofre Dy <janlofredy@gmail.com>
2024-09-30 17:14:28 +0800

    add end of travel

--- composables/Utilities.ts
+++ composables/Utilities.ts
@@ -101,6 +101,19 @@ export const dateToString = (date) => {
     return `${year}-${month}-${day}`
 }
 
+export const useFormatDateTimeString = (date: Date) => {
+    const year = date.getFullYear()
+    const month = String(date.getMonth() + 1).padStart(2, "0")
+    const day = String(date.getDate()).padStart(2, "0")
+    let hours = date.getHours()
+    const minutes = String(date.getMinutes()).padStart(2, "0")
+    const ampm = hours >= 12 ? "PM" : "AM"
+    hours = hours % 12
+    hours = hours ?? 12 // the hour "0" should be '12'
+    const formattedDate = `${year}-${month}-${day} ${hours}:${minutes} ${ampm}`
+    return formattedDate
+}
+
 export const useFormatCurrency = (number: number | string, locale = "en-US") => {
     const formatter = new Intl.NumberFormat(locale, {
         style: "decimal",
