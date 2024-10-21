f8c229a6629912748034d023f6f7ff53bd106a09
Leonardo Empuesto <brainsnap.2012@gmail.com>
2024-10-21 11:00:37 +0800

    show voucher nav on sidebar

--- components/layout/Nav/SideBar.vue
+++ components/layout/Nav/SideBar.vue
@@ -719,7 +719,6 @@ const config = useRuntimeConfig()
                 title="Vouchers"
             >
                 <LayoutNavSingle
-                    v-show="config.public.APP_ENV == 'local'"
                     v-if="useCheckAccessibility([
                         AccessibilityTypes.accounting_account_groups,
                     ])"
@@ -728,7 +727,6 @@ const config = useRuntimeConfig()
                     single-nav-title="Disbursement Voucher"
                 />
                 <LayoutNavSingle
-                    v-show="config.public.APP_ENV == 'local'"
                     v-if="useCheckAccessibility([
                         AccessibilityTypes.accounting_account_groups,
                     ])"
