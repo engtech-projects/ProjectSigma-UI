f7b4c2501c11d63295716109825e81808cbfaff0
prosbite <brainsnap.2012@gmail.com>
2024-11-11 12:11:45 +0800

    some changes

--- stores/accounting/paymentrequest.ts
+++ stores/accounting/paymentrequest.ts
@@ -14,9 +14,7 @@ export const usePaymentRequestStore = defineStore("paymentRequestStore", {
         list: [],
         pagination: {},
         getParams: {},
-        params: {
-            status: ""
-        },
+        params: {},
         errorMessage: "",
         successMessage: "",
         isLoading: {
