f7cabb5f3a41b818039eaa33a0aa91ca3da64e44
DanielDoysabas <124086485+DanielDoysabas@users.noreply.github.com>
2024-11-26 05:24:14 +0800

    [Update] - New Item Profile > Fields Restructure

--- components/inventory/Common/Table/ItemProfile/ItemProfileEdit.vue
+++ components/inventory/Common/Table/ItemProfile/ItemProfileEdit.vue
@@ -1,24 +1,9 @@
 <script setup lang="ts">
-const props = defineProps({
-    index: {
-        type: Number,
-        required: true,
-    },
-    uomTypes: {
-        type: Object,
-        required: true,
-    },
-    inventoryTypes: {
-        type: Array,
-        required: true,
-    },
-})
-const { uomTypes } = props
 const emit = defineEmits(["doEditItem", "doHideEditItem", "itemGroupItem"])
 const doHideItem = () => {
     emit("doHideEditItem")
 }
-const itemProfile = defineModel("itemProfile", { required: true, type: String, default: null })
+const itemProfile = defineModel("itemProfile", { required: true, type: Object, default: null })
 
 function shuffleString (str:String) {
     const chars = str.split("")
@@ -37,20 +22,13 @@ const showSuggest = (itemProfile:any) => {
         item_code: itemProfile.item_code,
         item_description: itemProfile.item_description,
         thickness_val: itemProfile.thickness_val,
-        thickness_uom: itemProfile.thickness_uom,
         length_val: itemProfile.length_val,
-        length_uom: itemProfile.length_uom,
         width_val: itemProfile.width_val,
-        width_uom: itemProfile.width_uom,
         height_val: itemProfile.height_val,
-        height_uom: itemProfile.height_uom,
         outside_diameter_val: itemProfile.outside_diameter_val,
-        outside_diameter_uom: itemProfile.outside_diameter_uom,
         inside_diameter_val: itemProfile.inside_diameter_val,
-        inside_diameter_uom: itemProfile.inside_diameter_uom,
         specification: itemProfile.specification,
         volume_val: itemProfile.volume_val,
-        volume_uom: itemProfile.volume_uom,
         grade: itemProfile.grade,
         color: itemProfile.color,
     }
@@ -90,54 +68,71 @@ const selectSuggest = (item:any, itemProfile:any) => {
     </td>
     <td colspan="1" class="px-2 py-2 border-0 border-b border-r font-medium text-gray-900 whitespace-nowrap text-center">
         <div class="flex flex-row">
-            <InventoryCommonFormPsNumberInput v-model="itemProfile.thickness_val" title="Thickness" />
-            <InventoryCommonFormPsSelect v-model="itemProfile.thickness_uom" title="Type" :select-list="uomTypes.allType" />
+            <InventoryCommonFormPsTextInput v-model="itemProfile.thickness_val" title="Thickness" />
         </div>
     </td>
     <td colspan="1" class="px-2 py-2 border-0 border-b border-r font-medium text-gray-900 whitespace-nowrap text-center">
         <div class="flex flex-row">
-            <InventoryCommonFormPsNumberInput v-model="itemProfile.length_val" title="Length" />
-            <InventoryCommonFormPsSelect v-model="itemProfile.length_uom" title="Type" :select-list="uomTypes.allType" />
+            <InventoryCommonFormPsTextInput v-model="itemProfile.length_val" title="Length" />
         </div>
     </td>
     <td colspan="1" class="px-2 py-2 border-0 border-b border-r font-medium text-gray-900 whitespace-nowrap text-center">
         <div class="flex flex-row">
-            <InventoryCommonFormPsNumberInput v-model="itemProfile.width_val" title="Width" />
-            <InventoryCommonFormPsSelect v-model="itemProfile.width_uom" title="Type" :select-list="uomTypes.allType" />
+            <InventoryCommonFormPsTextInput v-model="itemProfile.width_val" title="Width" />
         </div>
     </td>
     <td colspan="1" class="px-2 py-2 border-0 border-b border-r font-medium text-gray-900 whitespace-nowrap text-center">
         <div class="flex flex-row">
-            <InventoryCommonFormPsNumberInput v-model="itemProfile.height_val" title="Height" />
-            <InventoryCommonFormPsSelect v-model="itemProfile.height_uom" title="Type" :select-list="uomTypes.allType" />
+            <InventoryCommonFormPsTextInput v-model="itemProfile.height_val" title="Height" />
         </div>
     </td>
     <td colspan="1" class="px-2 py-2 border-0 border-b border-r font-medium text-gray-900 whitespace-nowrap text-center">
         <div class="flex flex-row">
-            <InventoryCommonFormPsNumberInput v-model="itemProfile.outside_diameter_val" title="Outside Diameter" />
-            <InventoryCommonFormPsSelect v-model="itemProfile.outside_diameter_uom" title="Type" :select-list="uomTypes.allType" />
+            <InventoryCommonFormPsTextInput v-model="itemProfile.outside_diameter_val" title="Outside Diameter" />
         </div>
     </td>
     <td colspan="1" class="px-2 py-2 border-0 border-b border-r font-medium text-gray-900 whitespace-nowrap text-center">
         <div class="flex flex-row">
-            <InventoryCommonFormPsNumberInput v-model="itemProfile.inside_diameter_val" title="Inside Diameter" />
-            <InventoryCommonFormPsSelect v-model="itemProfile.inside_diameter_uom" title="Type" :select-list="uomTypes.allType" />
+            <InventoryCommonFormPsTextInput v-model="itemProfile.inside_diameter_val" title="Inside Diameter" />
+        </div>
+    </td>
+    <td colspan="1" class="px-2 py-2 border-0 border-b border-r font-medium text-gray-900 whitespace-nowrap text-center">
+        <div class="flex flex-row">
+            <InventoryCommonFormPsTextInput v-model="itemProfile.angle" title="Angle" />
+        </div>
+    </td>
+    <td colspan="1" class="px-2 py-2 border-0 border-b border-r font-medium text-gray-900 whitespace-nowrap text-center">
+        <div class="flex flex-row">
+            <InventoryCommonFormPsTextInput v-model="itemProfile.size" title="Size" />
         </div>
     </td>
     <td colspan="1" class="px-2 py-2 border-0 border-b border-r font-medium text-gray-900 whitespace-nowrap text-center">
         <div class="flex flex-row">
             <InventoryCommonFormPsTextInput v-model="itemProfile.volume" title="Volume" />
-            <InventoryCommonFormPsSelect v-model="itemProfile.volume_uom" title="Type" :select-list="uomTypes.allType" />
         </div>
     </td>
     <td colspan="1" class="px-2 py-2 border-0 border-b border-r font-medium text-gray-900 whitespace-nowrap text-center">
-        <InventoryCommonFormPsTextInput v-model="itemProfile.specification" title="Specification" />
+        <div class="flex flex-row">
+            <InventoryCommonFormPsTextInput v-model="itemProfile.weight" title="Weight" />
+        </div>
     </td>
     <td colspan="1" class="px-2 py-2 border-0 border-b border-r font-medium text-gray-900 whitespace-nowrap text-center">
         <InventoryCommonFormPsTextInput v-model="itemProfile.grade" title="Grade" />
     </td>
     <td colspan="1" class="px-2 py-2 border-0 border-b border-r font-medium text-gray-900 whitespace-nowrap text-center">
+        <InventoryCommonFormPsTextInput v-model="itemProfile.volts" title="Volts" />
+    </td>
+    <td colspan="1" class="px-2 py-2 border-0 border-b border-r font-medium text-gray-900 whitespace-nowrap text-center">
+        <InventoryCommonFormPsTextInput v-model="itemProfile.plates" title="Plates" />
+    </td>
+    <td colspan="1" class="px-2 py-2 border-0 border-b border-r font-medium text-gray-900 whitespace-nowrap text-center">
+        <InventoryCommonFormPsTextInput v-model="itemProfile.part_number" title="Part Number" />
+    </td>
+    <td colspan="1" class="px-2 py-2 border-0 border-b border-r font-medium text-gray-900 whitespace-nowrap text-center">
         <InventoryCommonFormPsTextInput v-model="itemProfile.color" title="Color" />
+    </td>
+    <td colspan="1" class="px-2 py-2 border-0 border-b border-r font-medium text-gray-900 whitespace-nowrap text-center">
+        <InventoryCommonFormPsTextInput v-model="itemProfile.specification" title="Specification" />
     </td>
     <td colspan="1" class="px-2 py-2 border-0 border-b border-r font-medium text-gray-900 whitespace-nowrap text-center">
         <InventoryItemProfileUOMSelect v-model="itemProfile.uom" title="UOM Type" />
@@ -146,10 +141,15 @@ const selectSuggest = (item:any, itemProfile:any) => {
         <InventoryCommonFormPsSelectItemGroup v-model="itemProfile.item_group" title="Item Group" />
     </td>
     <td colspan="1" class="px-2 py-2 border-0 border-b border-r font-medium text-gray-900 whitespace-nowrap text-center">
-        <InventoryCommonFormPsSelectSubItemGroup v-model="itemProfile.sub_item_group" :item-group="itemProfile.item_group" title="Item Group" />
+        <InventoryCommonFormPsSelectSubItemGroup v-model="itemProfile.sub_item_group" :item-group="itemProfile.item_group" title="Sub Item Group" />
     </td>
     <td colspan="1" class="px-2 py-2 border-0 border-b border-r font-medium text-gray-900 whitespace-nowrap text-center">
-        <InventoryCommonFormPsSelect v-model="itemProfile.inventory_type" title="Inventory Type" :select-list="props.inventoryTypes" />
+        <LayoutFormPsSelect
+            v-model="itemProfile.inventory_type"
+            :options-list="['Inventoriable', 'Non-Inventoriable']"
+            class="bg-gray-80 border border-gray-300 text-gray-900 sm:text-sm focus:ring-primary-600 focus:border-primary-600 block"
+            title="Inventory Type"
+        />
     </td>
     <td class="flex flex-row justify-center items-center px-2 font-medium text-gray-900 whitespace-nowrap text-center">
         <div class="cancel-control">
