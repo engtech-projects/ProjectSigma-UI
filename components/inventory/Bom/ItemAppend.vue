00c66f7206cee7c3e3bdee317339b7574d5dbb9e
DanielDoysabas <124086485+DanielDoysabas@users.noreply.github.com>
2024-12-12 14:40:09 +0800

    [FIX] - BOM

--- components/inventory/Bom/ItemAppend.vue
+++ components/inventory/Bom/ItemAppend.vue
@@ -11,9 +11,43 @@ const doRemoveItem = (item:any) => {
 }
 const item = defineModel("item", { required: true, type: Object, default: null })
 const compId = useId()
+const amount = computed(() => {
+    const total = item.value.unit_price * item.value.quantity
+    return total
+})
+
+const watchUnitChange = (thisItem:any) => {
+    if (item.value.conversion) {
+        const baseValue = parseFloat(item.value.quantity) / parseFloat(item.value.conversion)
+        item.value.conversion = thisItem.conversion
+        item.value.quantity = baseValue * item.value.conversion
+    }
+    item.value.conversion = thisItem.conversion
+}
 </script>
 <template>
     <tr class="border-b-2 border-gray-300">
+        <td colspan="1" class="px-2 py-2 border-0 border-b border-r font-medium text-gray-900 whitespace-nowrap">
+            <InventoryBomItemSelector
+                v-model="item.item_id"
+            />
+        </td>
+        <td colspan="1" class="px-2 py-2 border-0 border-b border-r font-medium text-gray-900 whitespace-nowrap">
+            <InventoryBomItemUomSelector
+                :id="compId"
+                v-model="item.uom_id"
+                @watch-item="watchUnitChange"
+            />
+        </td>
+        <td colspan="1" class="px-2 py-2 border-0 border-b border-r font-medium text-gray-900 whitespace-nowrap text-center">
+            <input
+                :id="compId"
+                v-model="item.unit_price"
+                type="number"
+                class="w-full min-w-[120px] border border-slate-600 rounded-md px-3 text-md flex items-center relative cursor-pointer"
+                required
+            >
+        </td>
         <td colspan="1" class="px-2 py-2 border-0 border-b border-r font-medium text-gray-900 whitespace-nowrap text-center">
             <input
                 :id="compId"
@@ -24,67 +58,7 @@ const compId = useId()
             >
         </td>
         <td colspan="1" class="px-2 py-2 border-0 border-b border-r font-medium text-gray-900 whitespace-nowrap text-center">
-            <input
-                :id="compId"
-                v-model="item.unit"
-                type="number"
-                class="w-full min-w-[120px] border border-slate-600 rounded-md px-3 text-md flex items-center relative cursor-pointer"
-                required
-            >
-        </td>
-        <td colspan="1" class="px-2 py-2 border-0 border-b border-r font-medium text-gray-900 whitespace-nowrap text-center">
-            <input
-                :id="compId"
-                v-model="item.sku"
-                type="number"
-                class="w-full min-w-[120px] border border-slate-600 rounded-md px-3 text-md flex items-center relative cursor-pointer"
-                required
-            >
-        </td>
-        <td colspan="1" class="px-2 py-2 border-0 border-b border-r font-medium text-gray-900 whitespace-nowrap text-center">
-            <input
-                :id="compId"
-                v-model="item.item_description"
-                type="number"
-                class="w-full min-w-[120px] border border-slate-600 rounded-md px-3 text-md flex items-center relative cursor-pointer"
-                required
-            >
-        </td>
-        <td colspan="1" class="px-2 py-2 border-0 border-b border-r font-medium text-gray-900 whitespace-nowrap text-center">
-            <input
-                :id="compId"
-                v-model="item.specification"
-                type="number"
-                class="w-full min-w-[120px] border border-slate-600 rounded-md px-3 text-md flex items-center relative cursor-pointer"
-                required
-            >
-        </td>
-        <td colspan="1" class="px-2 py-2 border-0 border-b border-r font-medium text-gray-900 whitespace-nowrap text-center">
-            <input
-                :id="compId"
-                v-model="item.preferred_brand"
-                type="number"
-                class="w-full min-w-[120px] border border-slate-600 rounded-md px-3 text-md flex items-center relative cursor-pointer"
-                required
-            >
-        </td>
-        <td colspan="1" class="px-2 py-2 border-0 border-b border-r font-medium text-gray-900 whitespace-nowrap text-center">
-            <input
-                :id="compId"
-                v-model="item.reason_for_requests"
-                type="number"
-                class="w-full min-w-[120px] border border-slate-600 rounded-md px-3 text-md flex items-center relative cursor-pointer"
-                required
-            >
-        </td>
-        <td colspan="1" class="px-2 py-2 border-0 border-b border-r font-medium text-gray-900 whitespace-nowrap text-center">
-            <input
-                :id="compId"
-                v-model="item.turn_over"
-                type="number"
-                class="w-full min-w-[120px] border border-slate-600 rounded-md px-3 text-md flex items-center relative cursor-pointer"
-                required
-            >
+            {{ amount ? amount : 0 }}
         </td>
         <td class="flex flex-row justify-center items-center px-2 font-medium text-gray-900 whitespace-nowrap text-center">
             <div class="cancel-control">
