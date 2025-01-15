88c72a3587f62e2df37a9143f19d4667f7ca1a7c
DanielDoysabas <124086485+DanielDoysabas@users.noreply.github.com>
2025-01-15 14:43:55 +0800

    [FIX] - Loan/OtherDeduction Reports

---
+++ components/hrms/Reports/LoanReports/HdmfReportHeader.vue
@@ -0,0 +1,47 @@
+<script lang="ts" setup>
+const compId = useId()
+</script>
+<template>
+    <div :for="compId" class="header flex flex-col  mb-8">
+        <div class="flex gap-4">
+            <span class="text-md flex-1">
+                Employer ID:
+            </span>
+            <span class="text-md font-bold flex-5">
+                209658570002
+            </span>
+        </div>
+        <div class="flex gap-4">
+            <span class="text-md flex-1">
+                Employer Name:
+            </span>
+            <span class="text-md font-bold flex-5">
+                EVENPAR CONSTRUCTION AND DEVELOPMENT CORPORATION
+            </span>
+        </div>
+        <div class="flex gap-4">
+            <span class="text-md flex-1">
+                Address:
+            </span>
+            <span class="text-md font-bold flex-5">
+                P-1 POBLACION 1 BUENAVISTA AGUSAN DEL NORTE
+            </span>
+        </div>
+        <div class="flex gap-4">
+            <span class="text-md flex-1">
+                Contact No:
+            </span>
+            <span class="text-md font-bold flex-5">
+                09395096694
+            </span>
+        </div>
+        <div class="flex gap-4">
+            <span class="text-md flex-1">
+                Email Address:
+            </span>
+            <span class="text-md font-bold flex-5 underline">
+                evenparcorporation@gmail.com
+            </span>
+        </div>
+    </div>
+</template>
