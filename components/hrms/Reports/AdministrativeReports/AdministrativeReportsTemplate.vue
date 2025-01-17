36968f77717898de83131ea710f442b98631aba1
DanielDoysabas <124086485+DanielDoysabas@users.noreply.github.com>
2025-01-17 14:18:05 +0800

    Update AdministrativeReportsTemplate.vue

--- components/hrms/Reports/AdministrativeReports/AdministrativeReportsTemplate.vue
+++ components/hrms/Reports/AdministrativeReports/AdministrativeReportsTemplate.vue
@@ -15,10 +15,7 @@ const { administrativeReportOption } = storeToRefs(generateReportstore)
                     class="w-full bg-gray-50 border border-gray-300 text-gray-900 text-md rounded-lg focus:ring-blue-500 focus:border-blue-500 block p-2 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
                 >
                     <option value="employee-tenureship">
-                        Employee Tenurship
-                    </option>
-                    <option value="employee-masterlist">
-                        Employee Master List
+                        Employee Tenureship
                     </option>
                 </select>
             </div>
