7cb0ec3c64f1af821f02cdf155feba6bfd14b25e
DanielDoysabas <124086485+DanielDoysabas@users.noreply.github.com>
2025-01-30 17:20:18 +0800

    [Update] - Administrative Masterlist Export Report

---
+++ components/hrms/Reports/AdministrativeReports/AdministrativeExcelExportButton.vue
@@ -0,0 +1,23 @@
+<script lang="ts" setup>
+import { useGenerateReportStore } from "@/stores/hrms/reports/generateReport"
+const generateReportstore = useGenerateReportStore()
+const { administrativeReports } = storeToRefs(generateReportstore)
+const compId = useId()
+const config = useRuntimeConfig()
+</script>
+<template>
+    <div :for="compId" class="gap-1">
+        <a
+            type="button"
+            class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2 text-center"
+            target="_blank"
+            :href="config.public.HRMS_API_URL + '/api/administrative-export?report_type='+ administrativeReports.params.report_type + '&group_type=' + administrativeReports.params.group_type"
+        >
+            <Icon
+                name="mage:file-download-fill"
+                class="h-5 w-5 lg:h-5 lg:w-5"
+            />
+            Excel Export
+        </a>
+    </div>
+</template>
