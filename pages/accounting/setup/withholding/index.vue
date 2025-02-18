666ce0969ff2e14141cc1bdd6303caa11c34df20
Leonardo Empuesto <brainsnap.2012@gmail.com>
2025-02-18 11:25:41 +0800

    initialized

---
+++ pages/accounting/setup/withholding/index.vue
@@ -0,0 +1,38 @@
+<script setup>
+import { useAccountStore } from "@/stores/accounting/setup/account"
+import { useAccountTypeStore } from "@/stores/accounting/setup/accounttype"
+import { useReportGroupStore } from "@/stores/accounting/setup/reportgroup"
+
+const accountStore = useAccountStore()
+const accountTypeStore = useAccountTypeStore()
+const reportGroupStore = useReportGroupStore()
+accountTypeStore.getAccountTypes()
+reportGroupStore.getReportGroups()
+accountStore.getAccounts()
+
+useHead({
+    title: "Account",
+})
+
+</script>
+<template>
+    <LayoutAcessContainer
+        :if-access="useCheckAccessibility([
+            AccessibilityTypes.ACCOUNTING_SETUP_ACCOUNTS,
+        ])"
+    >
+        <div class="flex flex-col md:flex-row gap-4">
+            <AccountingSetupAccountCreate
+                v-show="!accountStore.isEdit"
+                class="flex-1"
+            />
+            <AccountingSetupAccountEdit v-show="accountStore.isEdit" class="flex-1" />
+            <AccountingSetupWithholdingList class="flex-2" />
+        </div>
+    </LayoutAcessContainer>
+</template>
+<style scoped>
+.flex-2 {
+    flex: 2;
+}
+</style>
