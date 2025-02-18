2a6753ad1eec61b76bbb136ba0255f23899df12f
Jose Janlofre Dy <janlofredy@gmail.com>
2025-02-18 14:58:13 +0800

    Update DetailsTable.vue

--- components/hrms/payroll/SalaryDisbursement/DetailsTable.vue
+++ components/hrms/payroll/SalaryDisbursement/DetailsTable.vue
@@ -491,7 +491,7 @@ const uniqueLoanNameTotals = computed(() => {
                     </tr>
                 </tbody>
             </table>
-            <LayoutApprovalsListView :approvals="data.approvals" :is-approved="data.request_status.toLowerCase() === 'approved'" />
+            <LayoutApprovalsListView :approvals="data.approvals" :is-approved="data.request_status?.toLowerCase() === 'approved'" />
         </div>
     </LayoutPrint>
 </template>
