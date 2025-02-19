416d22a8f0bd983f219f84e6b027bc3b569cbd5d
J <115533073+jbanban@users.noreply.github.com>
2025-02-19 10:13:08 +0800

    update 201 missing fields

--- components/hrms/Employee/201/Details/EmployeeInfo.vue
+++ components/hrms/Employee/201/Details/EmployeeInfo.vue
@@ -78,14 +78,12 @@ const { information, editable } = storeToRefs(employee)
                         <input id="employee_mobile_number" v-model="information.mobile_number" type="text" class="block w-full p-1 text-gray-900 border border-gray-300 rounded-md bg-gray-50 sm:text-xs focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" :disabled="!editable">
                     </div>
                 </td>
-                <td class="border border-slate-300 p-1">
+                <!-- <td class="border border-slate-300 p-1">
                     <div>
                         <label for="employee_telephone_number" class="block mb-2 text-[11px] font-medium text-gray-900 dark:text-white">LANDLINE</label>
                         <input id="employee_telephone_number" v-model="information.telephone_number" type="text" class="block w-full p-1 text-gray-900 border border-gray-300 rounded-md bg-gray-50 sm:text-xs focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" :disabled="!editable">
                     </div>
-                </td>
-            </tr>
-            <tr>
+                </td> -->
                 <td class="border border-slate-300 p-1">
                     <div>
                         <label for="employee_date_of_birth" class="block mb-2 text-[11px] font-medium text-gray-900 dark:text-white">DATE OF BIRTH</label>
@@ -104,52 +102,14 @@ const { information, editable } = storeToRefs(employee)
                         <input id="employee_citizenship" v-model="information.citizenship" type="text" class="block w-full p-1 text-gray-900 border border-gray-300 rounded-md bg-gray-50 sm:text-xs focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" :disabled="!editable">
                     </div>
                 </td>
-                <td class="grid grid-cols-2 gap-1 border-slate-300 p-1">
+                <td class="flex-1 space-y-2 border-slate-300 p-1">
                     <div>
                         <label for="employee_blood_type" class="block mb-2 text-[11px] font-medium text-gray-900 dark:text-white">BLOOD TYPE</label>
-                        <select id="employee_blood_type" v-model="information.blood_type" name="bloodtype" class="block w-full p-0 text-gray-900 border border-gray-300 rounded-md bg-gray-50 dark:text-gray-400 dark:bg-gray-700 dark:border-gray-600 focus:outline-none dark:placeholder-gray-400 text-sm" :disabled="!editable">
-                            <option value="A+">
-                                A+
-                            </option>
-                            <option value="A-">
-                                A-
-                            </option>
-                            <option value="B+">
-                                B+
-                            </option>
-                            <option value="B-">
-                                B-
-                            </option>
-                            <option value="O+">
-                                O+
-                            </option>
-                            <option value="O">
-                                O
-                            </option>
-                            <option value="O-">
-                                O-
-                            </option>
-                            <option value="AB+">
-                                AB+
-                            </option>
-                            <option value="AB-">
-                                AB-
-                            </option>
-                        </select>
+                        <input id="employee_blood_type" v-model="information.blood_type" type="text" class="block w-full p-1 text-gray-900 border border-gray-300 rounded-md bg-gray-50 sm:text-xs focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" :disabled="!editable">
                     </div>
                     <div>
                         <label for="employee_gender" class="block mb-2 text-[11px] font-medium text-gray-900 dark:text-white">GENDER</label>
-                        <select id="employee_gender" v-model="information.gender" name="gender" class="block w-full p-0 text-gray-900 border border-gray-300 rounded-md bg-gray-50 dark:text-gray-400 dark:bg-gray-700 dark:border-gray-600 focus:outline-none dark:placeholder-gray-400 text-sm" :disabled="!editable">
-                            <option value="MALE">
-                                Male
-                            </option>
-                            <option value="FEMALE">
-                                Female
-                            </option>
-                            <option value="OTHER">
-                                Other
-                            </option>
-                        </select>
+                        <input id="employee_gender" v-model="information.gender" type="text" class="block w-full p-1 text-gray-900 border border-gray-300 rounded-md bg-gray-50 sm:text-xs focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" :disabled="!editable">
                     </div>
                 </td>
             </tr>
@@ -163,21 +123,10 @@ const { information, editable } = storeToRefs(employee)
                 <td class="flex-1 space-y-2 border-slate-300 p-1">
                     <div>
                         <label for="employee_civil_status" class="block mb-2 text-[11px] font-medium text-gray-900 dark:text-white">CIVIL STATUS</label>
-                        <select id="employee_civil_status" v-model="information.civil_status" name="civilstatus" class="block w-full p-0 text-gray-900 border border-gray-300 rounded-md bg-gray-50 dark:text-gray-400 dark:bg-gray-700 dark:border-gray-600 focus:outline-none dark:placeholder-gray-400 text-sm" :disabled="!editable">
-                            <option value="SINGLE">
-                                SINGLE
-                            </option>
-                            <option value="MARRIED">
-                                MARRIED
-                            </option>
-                            <option value="WIDOW">
-                                WIDOW
-                            </option>
-                            <option value="COMPLICATED">
-                                COMPLICATED
-                            </option>
-                        </select>
+                        <input id="employee_civil_status" v-model="information.civil_status" type="text" class="block w-full p-1 text-gray-900 border border-gray-300 rounded-md bg-gray-50 sm:text-xs focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" :disabled="!editable">
                     </div>
+                </td>
+                <td class="border border-slate-300 p-1">
                     <div>
                         <label for="employee_date_of_marriage" class="block mb-2 text-[11px] font-medium text-gray-900 dark:text-white">DATE OF MARRIAGE</label>
                         <input id="employee_date_of_marriage" v-model="information.date_of_marriage" type="date" class="block w-full p-1 text-gray-900 border border-gray-300 rounded-md bg-gray-50 sm:text-xs focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" :disabled="!editable">
