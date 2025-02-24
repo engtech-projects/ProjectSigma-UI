4d2b42ace94702d300aded588cd4384194ae3848
DanielDoysabas <124086485+DanielDoysabas@users.noreply.github.com>
2025-02-24 15:10:27 +0800

    [Update] - Dashboard > Lates This Month, Absences This Month

--- components/hrms/Dashboard/Absences/AbsencesCard.vue
+++ components/hrms/Dashboard/Absences/AbsencesCard.vue
@@ -1,23 +1,25 @@
 <script setup>
 import { useDashboardStatisticsStore } from "@/stores/hrms/dashboardStats"
 const stats = useDashboardStatisticsStore()
-const { monthlyAbsences } = storeToRefs(stats)
+const { monthlyLateAbsences } = storeToRefs(stats)
 onMounted(() => {
-    stats.getMonthlyAbsences()
+    if (!monthlyLateAbsences.value.isLoaded) {
+        stats.getMonthlyLateAbsences()
+    }
 })
 </script>
 <template>
-    <LayoutBoards title="Absences This Month" :loading="monthlyAbsences.isLoading">
+    <LayoutBoards title="Absences This Month" :loading="monthlyLateAbsences.isLoading">
         <div class="grid grid-cols-3 md:grid-cols-4 justify-start gap-4 p-2 max-h-96 overflow-y-scroll">
-            <div v-if="monthlyAbsences.list.length <= 0">
+            <div v-if="monthlyLateAbsences.list.length <= 0">
                 No absences found this month.
             </div>
-            <div v-for="employee, index in monthlyAbsences.list" :key="index" class="flex flex-col items-center">
+            <div v-for="employee, index in monthlyLateAbsences.list" :key="index" class="flex flex-col items-center">
                 <HrmsDashboardAbsencesItem
                     title="Generate Memo"
                     :name="employee.fullname_first"
                     :avatar="employee.profile_photo && employee.profile_photo.base64 !== 'File doesn\'t exists.' ? employee.profile_photo.base64 : '/avatarexample.png'"
-                    :absences="employee.absent"
+                    :absences="employee.total_absents"
                 />
             </div>
         </div>
--- components/hrms/Dashboard/Lates/LatesCard.vue
+++ components/hrms/Dashboard/Lates/LatesCard.vue
@@ -1,23 +1,25 @@
 <script setup>
 import { useDashboardStatisticsStore } from "@/stores/hrms/dashboardStats"
 const stats = useDashboardStatisticsStore()
-const { monthlyLates } = storeToRefs(stats)
+const { monthlyLateAbsences } = storeToRefs(stats)
 onMounted(() => {
-    stats.getMonthlyLates()
+    if (!monthlyLateAbsences.value.isLoaded) {
+        stats.getMonthlyLateAbsences()
+    }
 })
 </script>
 <template>
-    <LayoutBoards title="Lates This Month" :loading="monthlyLates.isLoading">
+    <LayoutBoards title="Lates This Month" :loading="monthlyLateAbsences.isLoading">
         <div class="grid grid-cols-3 md:grid-cols-4 justify-start gap-4 p-2 max-h-96 overflow-y-scroll">
-            <div v-if="monthlyLates.list.length <= 0">
+            <div v-if="monthlyLateAbsences.list.length <= 0">
                 No Lates found this month.
             </div>
-            <div v-for="employee, index in monthlyLates.list" :key="index" class="flex flex-col items-center">
+            <div v-for="employee, index in monthlyLateAbsences.list" :key="index" class="flex flex-col items-center">
                 <HrmsDashboardLatesItem
                     title="Generate Memo"
                     :name="employee.fullname_first"
                     :avatar="employee.profile_photo && employee.profile_photo.base64 !== 'File doesn\'t exists.' ? employee.profile_photo.base64 : '/avatarexample.png'"
-                    :late="employee.lates"
+                    :late="employee.total_lates"
                 />
             </div>
         </div>
