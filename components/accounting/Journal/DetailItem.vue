dca34ed312e13bd0b9b883d788fc7da0bf8640a7
Leonardo Empuesto <brainsnap.2012@gmail.com>
2025-02-25 09:56:19 +0800

    Created stakeholder dropdown, other adjustments

--- components/accounting/Journal/DetailItem.vue
+++ components/accounting/Journal/DetailItem.vue
@@ -15,11 +15,14 @@ defineProps({
 })
 const details = defineModel("details", { required: true, type: Object })
 const emit = defineEmits("delete-item")
-
+const selectStakeholder = (stakeholder) => {
+    details.value.stakeholderInformation = stakeholder
+    details.value.stakeholder_id = stakeholder.id
+}
 </script>
 <template>
-    <div class="flex gap-2">
-        <div class="flex-1">
+    <div class="grid grid-cols-8 gap-2 p-2">
+        <div class="col-span-2 self-center">
             <label
                 for="account"
                 class="text-xs italic"
@@ -27,29 +30,29 @@ const emit = defineEmits("delete-item")
             <AccountingCommonSelectJournalAccounts
                 v-model:journal-account-info="details.journalAccountInfo"
                 v-model:account-id="details.account_id"
-                class="w-full min-w-[300px]"
             />
-            <!-- <div class="border border-black p-2">
-                {{ details.journalAccountInfo.account_name }}
-            </div> -->
         </div>
-        <div class="flex-1">
+        <div class="col-span-2 self-center">
             <label
                 for="particulars"
                 class="text-xs italic"
             >Project/Department</label>
-            <!-- <div v-if="details.stakeholderInformation" class="border border-black p-2">
-                {{ details.stakeholderInformation.name }}
-            </div> -->
-            <AccountingCommonSelectStakeHolder
+            <!-- <AccountingCommonSelectStakeHolder
                 v-model:stakeholder-info="details.stakeholderInformation"
                 v-model:selected-type="details.stakeholder_type"
-                class="w-full"
+                class="w-full bg-white"
                 :selected-id="details.stakeholder_id"
                 :filter-options="['project', 'department', 'employee']"
+            /> -->
+            <AccountingCommonSelectStakeholderSelect
+                :stakeholder-id="details.stakeholder_id"
+                :stakeholder="details.stakeholderInformation"
+                :style="'z-index:' + (10 + (itemCount - index))"
+                :select-options="['project', 'department', 'employee']"
+                @select="selectStakeholder"
             />
         </div>
-        <div class="flex-1">
+        <div class="col-span-1 self-center">
             <label
                 for="debit"
                 class="text-xs italic"
@@ -60,10 +63,10 @@ const emit = defineEmits("delete-item")
                 :disabled="details.credit != 0"
                 type="number"
                 step=".01"
-                class="w-full rounded-lg h-9 text-sm bg-gray-100"
+                class="w-full rounded-lg h-9 text-sm "
             >
         </div>
-        <div class="flex-1">
+        <div class="col-span-1 self-center">
             <label
                 for="credit"
                 class="text-xs italic"
@@ -74,31 +77,33 @@ const emit = defineEmits("delete-item")
                 :disabled="details.debit != 0"
                 type="number"
                 step=".01"
-                class="w-full rounded-lg h-9 text-sm bg-gray-100"
+                class="w-full rounded-lg h-9 text-sm "
             >
             <p v-if="(index === itemCount - 1) && details.debit === 0" class="text-italic text-xs text-blue-500 cursor-pointer" @click="details.credit = totalDebit">
                 max : {{ totalDebit }}
             </p>
         </div>
-        <div class="flex-1">
-            <label
-                for="remarks"
-                class="text-xs italic"
-            >Remarks/Description</label>
-            <input
-                id="remarks"
-                v-model="details.description"
-                type="text"
-                class="w-full rounded-lg h-9 text-sm bg-gray-100"
-            >
-        </div>
-        <div>
-            <button
-                class="text-white p-2 bg-red-500 content-center mt-5 rounded-md w-fit"
-                @click.prevent="emit('delete-item', index)"
-            >
-                <Icon name="ion:trash" color="white" class="bg-red-500 rounded h-8 w-8 p-1" />
-            </button>
+        <div class="flex gap-2 col-span-2 self-center">
+            <div class="">
+                <label
+                    for="remarks"
+                    class="text-xs italic"
+                >Remarks/Description</label>
+                <input
+                    id="remarks"
+                    v-model="details.description"
+                    type="text"
+                    class="w-full rounded-lg h-9 text-sm "
+                >
+            </div>
+            <div>
+                <button
+                    class="text-white p-2 bg-red-500 content-center mt-5 rounded-md w-fit"
+                    @click.prevent="emit('delete-item', index)"
+                >
+                    <Icon name="ion:trash" color="white" class="bg-red-500 rounded h-8 w-8 p-1" />
+                </button>
+            </div>
         </div>
     </div>
 </template>
---
+++ components/accounting/common/Select/StakeholderSelect.vue
@@ -0,0 +1,168 @@
+<template>
+    <div
+        ref="dropdownRef"
+        class="relative w-full flex flex-col border border-gray-500 rounded-lg min-h-[2.4rem]"
+    >
+        <div class="flex px-2 items-center flex-1 cursor-pointer" @click="toggleDropdown">
+            <span class="text-md flex-1 cursor-pointer select-none max-h-[2.4rem] overflow-hidden text-ellipsis whitespace-nowrap">{{ currentValue?.name || "Search Stakeholder" }}</span>
+            <Icon v-if="!showDD" name="mdi:chevron-down" class="" />
+            <Icon v-if="showDD" name="mdi:chevron-up" class="" />
+        </div>
+        <div v-if="showDD" class="absolute left-0 top-[37px] min-w-[100%] min-h-72 bg-white rounded-md border border-gray-500">
+            <div class="flex gap-1 w-full bg-gray-100 p-2 border-b">
+                <div class="flex flex-col w-[35%]">
+                    <label class="text-[9px] uppercase text-gray-700 mb-1">Dept</label>
+                    <select v-model="dept" class="border border-gray-500 rounded-md p-1 text-xs">
+                        <option v-for="option in selectOptions" :key="option" :value="option">
+                            {{ upperFirst(option) }}
+                        </option>
+                    </select>
+                </div>
+                <div class="flex flex-1 items-center gap-1">
+                    <div class="flex flex-col pb-2 w-full">
+                        <label class="text-[9px] uppercase text-gray-700 mb-1 cursor-pointer select-none">Stakeholder</label>
+                        <div class="flex items-center gap-1 border border-gray-500 rounded-md">
+                            <input
+                                v-if="dept"
+                                v-model="searchString"
+                                type="text"
+                                class="border-none rounded-md p-1 text-xs focus:ring-0 flex-1"
+                                placeholder="Search Stakeholder"
+                                index="1"
+                            >
+                            <input
+                                v-else
+                                disabled
+                                type="text"
+                                class="border-none rounded-md p-1 text-xs flex-1"
+                                placeholder="Search Stakeholder"
+                                index="2"
+                                @focus="showDD = true"
+                            >
+                            <Icon name="mdi:close" class="h-5 w-5 font-bold cursor-pointer text-red-500 " :class="{ 'text-red-700': searchString, 'text-white': !searchString }" @click="searchString = ''" />
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="flex flex-col max-h-[200px] overflow-auto items-center">
+                <img
+                    v-if="isLoading"
+                    src="https://media4.giphy.com/media/v1.Y2lkPTc5MGI3NjExZGJ6eHh2dmZsMHN5ZnI0YTU5MGFocTk4djhxOXBvend4bG1jeCZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/sSgvbe1m3n93G/giphy.gif"
+                    alt=""
+                    srcset=""
+                    class="w-5 h-5"
+                >
+                <span v-if="results.length === 0 && !isLoading" class="text-xs text-gray-500 p-2">No result found</span>
+                <div class="flex flex-col items-start w-full">
+                    <span
+                        v-for="result in results"
+                        :key="result"
+                        class="text-xs text-gray-700 py-2 px-2 border-b hover:bg-gray-100 cursor-pointer w-full"
+                        :class="{ 'bg-green-200': result.id === currentValue.id }"
+                        @click="selectResult(result)"
+                    >{{ result.name }}</span>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script lang="ts" setup>
+const config = useRuntimeConfig()
+const props = defineProps({
+    stakeholderId: {
+        type: Number,
+        required: true
+    },
+    stakeholder: {
+        type: Object,
+        required: true
+    },
+    selectOptions: {
+        type: Array,
+        required: true
+    }
+})
+const isLoading = ref(false)
+const currentValue = ref(null)
+const showDD = ref(false)
+const emit = defineEmits(["select"])
+const dept = ref(null)
+const searchString = ref("")
+const searchStakeholder = async () => {
+    isLoading.value = true
+    try {
+        const response = await $fetch(config.public.ACCOUNTING_API_URL + "/api/search-stakeholders", {
+            method: "GET",
+            watch: true,
+            params: {
+                type: dept.value,
+                key: searchString.value,
+            },
+        })
+        isLoading.value = false
+        results.value = response.data?.data ?? []
+    } catch (err) {
+    }
+}
+const stakeholderType = computed(() => {
+    if (props.stakeholder) {
+        const parts = props.stakeholder?.stakeholdable_type?.split("\\")
+        return parts ? parts[parts.length - 1].toLowerCase() : ""
+    }
+    return ""
+})
+const results = ref([])
+let debounceTimer: string | number | NodeJS.Timeout | null | undefined = null
+// eslint-disable-next-line require-await
+watch(searchString, async (newQuery) => {
+    if (debounceTimer) { clearTimeout(debounceTimer) } // Clear previous timer
+
+    if (!newQuery) {
+        results.value = []
+        return
+    }
+
+    // Set a delay before executing search (e.g., 500ms)
+    debounceTimer = setTimeout(async () => {
+        await searchStakeholder()
+    }, 500)
+})
+watch(dept, async (newValue) => {
+    if (newValue && searchString.value) {
+        await searchStakeholder()
+    }
+})
+const dropdownRef = ref(null)
+const toggleDropdown = () => {
+    showDD.value = !showDD.value
+}
+
+// Close dropdown when clicking outside
+const handleClickOutside = (event) => {
+    if (dropdownRef.value && !dropdownRef.value.contains(event.target)) {
+        showDD.value = false
+        dept.value = stakeholderType.value
+    }
+}
+const selectResult = (result) => {
+    currentValue.value = result
+    emit("select", result)
+    showDD.value = false
+}
+onMounted(() => {
+    if (props.stakeholder) {
+        currentValue.value = props.stakeholder
+        // searchString.value = props.stakeholder.name
+        dept.value = stakeholderType.value
+    }
+    document.addEventListener("click", handleClickOutside)
+})
+
+onUnmounted(() => {
+    document.removeEventListener("click", handleClickOutside)
+})
+</script>
+
+<style>
+</style>
