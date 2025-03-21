c3a7290bd26fc5d17dd3db20e30f3307bf6c4d8c
DanielDoysabas <124086485+DanielDoysabas@users.noreply.github.com>
2025-03-21 13:39:32 +0800

    Update ApplicationForm.vue

--- components/hrms/Employee/Job/ApplicationForm.vue
+++ components/hrms/Employee/Job/ApplicationForm.vue
@@ -26,7 +26,7 @@ const submitForm = async () => {
             type: "success",
             text: jobapplicantstore.successMessage
         })
-        manpowers.getAllAvailableApplicant()
+        manpowers.reloadResources()
         closeViewModal()
     } catch (error) {
         snackbar.add({
