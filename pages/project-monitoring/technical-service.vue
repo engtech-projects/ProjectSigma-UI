94241ecadc36f11072d99cc8db566f62a000ca78
Leonardo Empuesto <brainsnap.2012@gmail.com>
2025-03-25 10:15:22 +0800

    Minor changes

--- pages/project-monitoring/technical-service.vue
+++ pages/project-monitoring/technical-service.vue
@@ -33,32 +33,7 @@ projectStore.viewState = false
                         <ProjectsAllList />
                     </AccountingCommonTabsTabContainer>
                     <AccountingCommonTabsTabContainer id="awardee">
-                        <AccountingCommonTabsMainContainer class="w-full">
-                            <template #tab-titles>
-                                <AccountingCommonTabsTabTitle
-                                    v-if="useCheckAccessibility([
-                                        AccessibilityTypes.ACCOUNTING_VOUCHER_DISBURSEMENT_ALL,
-                                    ])"
-                                    title="SUMMARY OF ESTIMATED NET INCOME"
-                                    target-id="summaryOfEstimatedNetIncome"
-                                />
-                                <AccountingCommonTabsTabTitle
-                                    v-if="useCheckAccessibility([
-                                        AccessibilityTypes.ACCOUNTING_VOUCHER_DISBURSEMENT_MY_APPROVAL,
-                                    ])"
-                                    title="SUMMARY OF ESTIMATED DIRECT COST"
-                                    target-id="summaryOfEstimatedDirectCost"
-                                />
-                            </template>
-                            <template #tab-containers>
-                                <AccountingCommonTabsTabContainer id="summaryOfEstimatedNetIncome">
-                                    <ProjectsAwardeeEstimatedNetIncome />
-                                </AccountingCommonTabsTabContainer>
-                                <AccountingCommonTabsTabContainer id="summaryOfEstimatedDirectCost">
-                                    <ProjectsAwardeeEstimatedDirectCost />
-                                </AccountingCommonTabsTabContainer>
-                            </template>
-                        </AccountingCommonTabsMainContainer>
+                        <ProjectsMydraftsDetails :awardee="true" />
                     </AccountingCommonTabsTabContainer>
                 </template>
             </AccountingCommonTabsMainContainer>
