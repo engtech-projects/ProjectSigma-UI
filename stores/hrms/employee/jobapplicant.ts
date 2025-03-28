e8a06b1ccf3e9bd18360e5915e43f9b826ba1e5d
Jose Janlofre Dy <janlofredy@gmail.com>
2025-03-28 13:11:59 +0800

    try fix job applicants

--- stores/hrms/employee/jobapplicant.ts
+++ stores/hrms/employee/jobapplicant.ts
@@ -376,7 +376,7 @@ export const useJobapplicantStore = defineStore("jobapplicants", {
             this.errorMessage = ""
             const formData = new FormData()
             Object.keys(this.jobapplicant).forEach((key) => {
-                if (!key.includes("attachment")) {
+                if (!key.includes("attachment") || ["workexperience", "education"].includes(key)) {
                     formData.append(key, JSON.stringify(this.jobapplicant[key]))
                 } else {
                     formData.append(key, this.jobapplicant[key])
