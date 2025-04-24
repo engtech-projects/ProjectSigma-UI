39cd2ebec464f389e033a17e15b408ca62f79fd2
DanielDoysabas <124086485+DanielDoysabas@users.noreply.github.com>
2025-04-24 14:12:17 +0800

    [Update] - Failure to log summary

---
+++ components/hrms/Reports/PortalMonitoring/FailureToLogMonitoringSummary.vue
@@ -0,0 +1,57 @@
+<script setup>
+import { useGenerateReportStore } from "@/stores/hrms/reports/generateReport"
+const generateReportstore = useGenerateReportStore()
+const { portalMonitoringReports } = storeToRefs(generateReportstore)
+const headers = [
+    { name: "NO", id: "" },
+    { name: "Employee Name", id: "employee_name" },
+    { name: "Total Number of Failure to Log Filled", id: "failuretolog_filled" },
+]
+</script>
+<template>
+    <div
+        v-if="useCheckAccessibility([AccessibilityTypes.HRMS_REPORTS_PORTALMONITORING_FAILURETOLOGSUMMARY]) || useCheckAccessibility(['AdminOnly'])"
+        class="flex flex-col"
+    >
+        <LayoutPrint>
+            <LayoutLoadingContainer :loading="portalMonitoringReports.isLoading">
+                <div class="h-full w-full">
+                    <div class="h-full">
+                        <table class="printTable table-auto border-collapse border border-gray-500">
+                            <thead class="text-blue-600 text-md">
+                                <tr>
+                                    <th
+                                        v-for="header in headers"
+                                        :key="header.name+'headerRow'"
+                                        class="p-1 border-gray-500 border"
+                                        :class="header.style ?? ''"
+                                    >
+                                        {{ header.name }}
+                                    </th>
+                                </tr>
+                            </thead>
+                            <tbody class="text-sm text-center">
+                                <tr v-for="failureToLogRequest, index in portalMonitoringReports.list" :key="'FailureToLogMonitoring' + index" class="h-2">
+                                    <td class="border border-gray-500 h-8">
+                                        {{ index + 1 }}
+                                    </td>
+                                    <td class="whitespace-nowrap border border-gray-500 h-8">
+                                        {{ failureToLogRequest.employee_name }}
+                                    </td>
+                                    <td class="border border-gray-500 h-8">
+                                        {{ failureToLogRequest.failuretolog_filled }}
+                                    </td>
+                                </tr>
+                            </tbody>
+                        </table>
+                    </div>
+                </div>
+            </LayoutLoadingContainer>
+        </LayoutPrint>
+    </div>
+</template>
+<style scoped>
+    .flex-5 {
+        flex: 5;
+    }
+</style>
