8c1803484b77f5e5a37fa28ce044864d5171ff76
Jose Janlofre Dy <35553424+janlofredy@users.noreply.github.com>
2025-04-24 11:50:25 +0800

    use tabs view

--- pages/hrms/payroll/13thmonthpay.vue
+++ pages/hrms/payroll/13thmonthpay.vue
@@ -1,20 +1,39 @@
+<script  setup lang="ts">
+const compId = useId()
+useHead({
+    title: "13th Month Pay",
+})
+</script>
 <template>
     <LayoutAcessContainer
         :if-access="useCheckAccessibility([
             AccessibilityTypes.hrms_payroll_13thmonth,
         ])"
     >
-        <div class="mt-5 edit-item w-full max-w-full bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700 p-6 overflow-auto">
-            <HrmsPayroll13thMonth13thmonthTable />
-            <HrmsPayroll13thMonth13thmonthForm />
+        <div class="grid grid-cols-1 md:flex-row gap-4">
+            <HrmsCommonTabsMainContainer>
+                <template #tab-titles>
+                    <HrmsCommonTabsTabTitle :target-id="'tab1' + compId" title="Generate 13th Month Pays" />
+                    <HrmsCommonTabsTabTitle :target-id="'tab2' + compId" title="All Requests" />
+                    <HrmsCommonTabsTabTitle :target-id="'tab3' + compId" title="My Requests" />
+                    <HrmsCommonTabsTabTitle :target-id="'tab4' + compId" title="My Approvals" />
+                </template>
+                <template #tab-containers>
+                    <HrmsCommonTabsTabContainer :id="'tab1' + compId">
+                        <HrmsPayroll13thMonth13thmonthTable />
+                        <HrmsPayroll13thMonth13thmonthForm />
+                    </HrmsCommonTabsTabContainer>
+                    <HrmsCommonTabsTabContainer :id="'tab2' + compId">
+                        <HrmsPayrollAllowanceAllRequests />
+                    </HrmsCommonTabsTabContainer>
+                    <HrmsCommonTabsTabContainer :id="'tab3' + compId">
+                        <HrmsPayrollAllowanceMyRequests />
+                    </HrmsCommonTabsTabContainer>
+                    <HrmsCommonTabsTabContainer :id="'tab4' + compId">
+                        <HrmsPayrollAllowanceMyApprovals />
+                    </HrmsCommonTabsTabContainer>
+                </template>
+            </HrmsCommonTabsMainContainer>
         </div>
     </LayoutAcessContainer>
 </template>
-
-<script  setup lang="ts">
-
-useHead({
-    title: "13th Month Pay",
-})
-
-</script>
