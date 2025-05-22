96ad514314d44c967bb82d127edfe55495b71fce
Leonardo Empuesto <brainsnap.2012@gmail.com>
2025-05-22 10:46:02 +0800

    proposal updates

---
+++ pages/project-monitoring/my-proposals.vue
@@ -0,0 +1,16 @@
+<script setup>
+import { useResourceStore } from "@/stores/project-monitoring/resource"
+const resourceStore = useResourceStore()
+resourceStore.getResourceUnits()
+</script>
+<template>
+    <LayoutAcessContainer
+        :if-access="useCheckAccessibility([
+            AccessibilityTypes.ACCOUNTING_VOUCHER_DISBURSEMENT_GROUP,
+        ])"
+    >
+        <div class="flex flex-col gap-4">
+            <ProjectsMydraftsDetails />
+        </div>
+    </LayoutAcessContainer>
+</template>
