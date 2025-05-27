7f4bb9f4c25e914a46b8c8eead5b28104dfdff64
eptals2 <eptals2@gmail.com>
2025-05-27 14:03:13 +0800

    Update BillOfQuantities.vue

--- components/projects/print/lgu/BillOfQuantities.vue
+++ components/projects/print/lgu/BillOfQuantities.vue
@@ -1,23 +1,39 @@
 <template>
     <!-- Header -->
     <div>
-        <h6 class="text-center">Department of Public Works and Highways</h6>
+        <h6 class="text-center">
+            Department of Public Works and Highways
+        </h6>
         <p>Contract ID: </p>
         <p>Contract Name: </p>
         <p>Contract Location: </p>
     </div>
 
     <div class="p-4">
-        <h2 class="text-xl font-bold mb-4">Bill of Quantities</h2>
+        <h2 class="text-xl font-bold mb-4">
+            Bill of Quantities
+        </h2>
         <table class="min-w-full border border-gray-300">
             <thead>
                 <tr class="bg-gray-200">
-                    <th class="border px-4 py-2">Pay Item No.</th>
-                    <th class="border px-4 py-2">Description</th>
-                    <th class="border px-4 py-2">Unit</th>
-                    <th class="border px-4 py-2">Quantity</th>
-                    <th class="border px-4 py-2">Unit Price(Pesos)</th>
-                    <th class="border px-4 py-2">Amount (Pesos)</th>
+                    <th class="border px-4 py-2">
+                        Pay Item No.
+                    </th>
+                    <th class="border px-4 py-2">
+                        Description
+                    </th>
+                    <th class="border px-4 py-2">
+                        Unit
+                    </th>
+                    <th class="border px-4 py-2">
+                        Quantity
+                    </th>
+                    <th class="border px-4 py-2">
+                        Unit Price(Pesos)
+                    </th>
+                    <th class="border px-4 py-2">
+                        Amount (Pesos)
+                    </th>
                 </tr>
             </thead>
             <tbody>
@@ -31,7 +47,9 @@
                 </tr>
             </tbody>
         </table>
-        <button @click="printTable" class="mt-4 px-4 py-2 bg-blue-600 text-white rounded">Print</button>
+        <button class="mt-4 px-4 py-2 bg-blue-600 text-white rounded"  @click="printTable">
+            Print
+        </button>
     </div>
 </template>
 
