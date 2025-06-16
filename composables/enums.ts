4e1feb425981413ed3bfee377ba92eeee8daaf5f
Leonardo Empuesto <brainsnap.2012@gmail.com>
2025-06-16 15:06:04 +0800

    document code updated

--- composables/enums.ts
+++ composables/enums.ts
@@ -1,5 +1,12 @@
 export const useAccountingEnums = () => {
     return {
         prf: "FM-ACS-001-1",
+        payroll_liquidation: "FM-TRS-001-1",
+        cash_return_slip: "FM-TRS-002-3",
+        provisional_receipt: "FM-TRS-002-2",
+        memorandum_of_deposit: "FM-TRS-002-1",
+        cash_advance_summary: "FM-TRS-001-3",
+        replenishment_summary: "FM-TRS-001-2",
+        liquidation_form: "FM-TRS-001-1",
     }
 }
