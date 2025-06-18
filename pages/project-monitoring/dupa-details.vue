badcbed47d04d8e17ce506fe1a99f812e5e6e7d5
Rustom Pedales <pedales.rustom@gmail.com>
2025-06-18 08:56:04 +0800

    [BugFix]: redeclare imports

--- pages/project-monitoring/dupa-details.vue
+++ pages/project-monitoring/dupa-details.vue
@@ -2,12 +2,10 @@
 import { useTaskStore } from "~/stores/project-monitoring/task"
 import { useProjectStore } from "~/stores/project-monitoring/projects"
 import { useResourceStore } from "~/stores/project-monitoring/resource"
-import { useProjectStore } from "~/stores/project-monitoring/projects"
 const route = useRoute()
 const taskStore = useTaskStore()
 const projectStore = useProjectStore()
 const resourceStore = useResourceStore()
-const projectStore = useProjectStore()
 await taskStore.getTask(route.query.id)
 await projectStore.getProject(route.query.pid)
 await resourceStore.getResourceNames()
