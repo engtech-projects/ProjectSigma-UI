f68b5801797a5745e3c75e6b451a8c776a1da2b9
Leonardo Empuesto <brainsnap.2012@gmail.com>
2025-06-27 10:15:59 +0800

    GANTT CHART, CASH FLOW, MANPOWER & EQUIPMENT UTILIZATION SCHEDULE

---
+++ components/projects/Gantt/GanttEquipment.vue
@@ -0,0 +1,66 @@
+<template>
+    <div class="mb-6">
+        <h1 class="text-md font-bold mb-2 block">
+            EQUIPMENT UTILIZATION SCHEDULE
+        </h1>
+        <div id="GanttChartDIVEquipment" />
+    </div>
+</template>
+
+<script>
+import * as JSGantt from "jsgantt-improved"
+import "jsgantt-improved/dist/jsgantt.css"
+
+export default {
+    name: "GanttChart",
+    props: {
+        msg: {
+            type: String,
+            default: ""
+        },
+    },
+    mounted () {
+        const g = new JSGantt.GanttChart(
+            document.getElementById("GanttChartDIVEquipment"),
+            "day"
+        )
+
+        g.setOptions({
+            vCaptionType: "Complete", // Set to Show Caption : None,Caption,Resource,Duration,Complete,
+            vQuarterColWidth: 20,
+            vDateTaskDisplayFormat: "day dd month yyyy", // Shown in tool tip box
+            vDayMajorDateDisplayFormat: "mon yyyy - Week ww", // Set format to dates in the "Major" header of the "Day" view
+            vWeekMinorDateDisplayFormat: "dd mon", // Set format to display dates in the "Minor" header of the "Week" view
+            vLang: "en",
+            vShowTaskInfoLink: 1, // Show link in tool tip (0/1)
+            vShowEndWeekDate: 0, // Show/Hide the date for the last day of the week in header for daily
+            vAdditionalHeaders: {
+                // Add data columns to your table
+                unit: {
+                    title: "",
+                }
+            },
+            vShowRes: 0, // Resource
+            vShowStartDate: 0, // Start Date
+            vShowEndDate: 0, // End Date
+            vShowDur: 0, // Duration
+            vShowComp: 0,
+            vUseSingleCell: 10000, // Set the threshold cell per table row (Helps performance for large data.
+            vFormatArr: ["Day", "Week", "Month", "Quarter"], // Even with setUseSingleCell using Hour format on such a large chart can cause issues in some browsers,
+        })
+
+        useGanttData().equipment.forEach((task) => {
+            g.AddTaskItemObject(task)
+        })
+
+        g.Draw()
+    },
+}
+</script>
+
+  <!-- Add "scoped" attribute to limit CSS to this component only -->
+  <style scoped>
+  h3 {
+    margin: 40px 0 0;
+  }
+  </style>
---
+++ pages/project-monitoring/gantt.vue
@@ -0,0 +1,26 @@
+<template>
+    <div class="mb-12">
+        <div v-if="!print">
+            <ProjectsGanttChart />
+            <ProjectsGanttEquipment />
+            <ProjectsGanttManpower />
+        </div>
+        <div v-if="!print" class="flex justify-end mb-6">
+            <button class="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded" @click="print = true">
+                <Icon name="iconoir:printer" /> Print
+            </button>
+        </div>
+        <LayoutPrint v-else>
+            <ProjectsGanttPrintMain />
+        </LayoutPrint>
+    </div>
+</template>
+
+<script setup lang="ts">
+const print = ref(false)
+</script>
+
+<style>
+#app {
+}
+</style>
