026dbd60da1547f4828b77e6b5b352848662e36c
eptals1 <eptals2@gmail.com>
2025-07-01 16:39:46 +0800

    Update index.vue

--- pages/project-monitoring/information/index.vue
+++ pages/project-monitoring/information/index.vue
@@ -1,11 +1,17 @@
 <template>
     <div class="flex flex-col gap-4">
         <ProjectsDetails :project-details="projectStore.information" />
+        <ProjectStageButton
+            v-if="projectStore.information.id"
+            :project-id="projectStore.information.id"
+            :current-stage="projectStore.information.stage"
+        />
     </div>
 </template>
 <script lang="ts" setup>
 import { useProjectStore } from "@/stores/project-monitoring/projects"
 import { useResourceStore } from "~/stores/project-monitoring/resource"
+import ProjectStageButton from "~/components/projects/ProjectsStageButton.vue"
 
 const route = useRoute()
 const projectId = route.query.id
