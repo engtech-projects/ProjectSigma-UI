2cf150183b49c9e5f9a67454041dc12514635080
Leonardo Empuesto <brainsnap.2012@gmail.com>
2025-07-11 15:20:22 +0800

    Update awarded.vue

--- pages/project-monitoring/marketing/awarded.vue
+++ pages/project-monitoring/marketing/awarded.vue
@@ -1,15 +1,3 @@
-<template>
-    <div class="px-8">
-        <div class="flex justify-between items-center">
-            <h1 class="text-2xl font-bold text-slate-600 my-8 text-left">
-                Awarded List Table
-            </h1>
-            <DropdownSearch />
-        </div>
-        <ProjectsMarketingAwardedList />
-    </div>
-</template>
-
 <script lang="ts" setup>
 import { useProjectStore } from "~/stores/project-monitoring/projects"
 const projectStore = useProjectStore()
@@ -21,7 +9,14 @@ useHead({
     title: "Awarded Projects",
 })
 </script>
-
-<style>
-
-</style>
+<template>
+    <div class="px-8">
+        <div class="flex justify-between items-center">
+            <h1 class="text-2xl font-bold text-slate-600 my-8 text-left">
+                Awarded List Table
+            </h1>
+            <DropdownSearch />
+        </div>
+        <ProjectsMarketingAwardedList />
+    </div>
+</template>
