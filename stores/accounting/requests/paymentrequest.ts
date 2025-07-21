926397e15b3aac0a760982d3491bcfc938d94772
Rustom Pedales <pedales.rustom@gmail.com>
2025-07-21 12:43:32 +0800

    Update paymentrequest.ts

--- stores/accounting/requests/paymentrequest.ts
+++ stores/accounting/requests/paymentrequest.ts
@@ -175,11 +175,11 @@ export const usePaymentRequestStore = defineStore("paymentRequestStore", {
                     onResponse: ({ response }) => {
                         this.myDeniedRequests.isLoading = false
                         if (response.ok) {
-                            this.myDeniedRequests.list = response._data.data.data
+                            this.myDeniedRequests.list = response._data.data
                             this.myDeniedRequests.pagination = {
-                                first_page: response._data.data.links.first,
-                                pages: response._data.data.meta.links,
-                                last_page: response._data.data.links.last,
+                                first_page: response._data.links.first,
+                                pages: response._data.meta.links,
+                                last_page: response._data.links.last,
                             }
                         }
                     },
