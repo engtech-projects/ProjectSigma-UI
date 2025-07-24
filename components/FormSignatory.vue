790a7c4926d9dfc15db83e7964aa7336f924b1cb
Leonardo Empuesto <brainsnap.2012@gmail.com>
2025-07-24 14:04:42 +0800

    print footers created

--- components/FormSignatory.vue
+++ components/FormSignatory.vue
@@ -15,6 +15,10 @@ defineProps({
     signature: {
         type: String,
         default: null
+    },
+    align: {
+        type: String,
+        default: "center"
     }
 })
 </script>
@@ -23,12 +27,15 @@ defineProps({
         <span class="">{{ label }} :</span>
         <div class="flex gap-8 flex-1">
             <!-- <img v-if="signature" :src="signature" class="w-48 h-48 object-cover rounded-md"> -->
-            <div v-if="!signatories" class="flex flex-col items-center gap-1">
+            <div v-if="!signatories" class="flex flex-col items-start gap-1" :class="align === 'left' ? 'items-start' : 'items-center'">
                 <span class="font-bold uppercase flex-1 border-b-2 border-black" :class="signatory?.name?.length === 0 ? 'w-48' : '' ">
                     {{ signatory?.name }}
                 </span>
                 <span class="flex-1">
                     {{ signatory?.title }}
+                </span>
+                <span class="flex-1">
+                    {{ signatory?.subtitle }}
                 </span>
             </div>
             <div v-for="s, i in signatories" v-else :key="i" class="relative flex flex-col items-center gap-1 justify-start">
@@ -38,6 +45,9 @@ defineProps({
                 </span>
                 <span class="flex-1">
                     {{ s?.title }}
+                </span>
+                <span class="flex-1">
+                    {{ s?.subtitle }}
                 </span>
             </div>
         </div>
