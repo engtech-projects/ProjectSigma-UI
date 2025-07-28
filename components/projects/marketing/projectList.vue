27c3563a0d77b13b597ff82ae4f91cf5f9f4439f
DanielDoysabas <124086485+DanielDoysabas@users.noreply.github.com>
2025-07-28 12:32:32 +0800

    Update projectList.vue

--- components/projects/marketing/projectList.vue
+++ components/projects/marketing/projectList.vue
@@ -46,7 +46,7 @@ const search = (value: string) => {
             <div class="flex flex-row justify-items-start items-start">
                 <div class="w-1/3 px-4">
                     <label for="status" class="block text-sm font-medium text-gray-700">Project Status</label>
-                    <select v-model="myProjectList.params.stage" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
+                    <select v-model="myProjectList.params.stage_status" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
                         <option :value="null" disabled selected>
                             - Select -
                         </option>
