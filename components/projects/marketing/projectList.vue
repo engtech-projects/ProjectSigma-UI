f66c4d5e53aee1c92f62c54635846f8a27442494
DanielDoysabas <124086485+DanielDoysabas@users.noreply.github.com>
2025-07-28 14:56:26 +0800

    [Update] - ProjectStatus Component

--- components/projects/marketing/projectList.vue
+++ components/projects/marketing/projectList.vue
@@ -35,30 +35,7 @@ const projectDetails = (data: any) => {
         >
             <div class="flex flex-row justify-items-start items-start">
                 <div class="w-1/3 px-4">
-                    <label for="status" class="block text-sm font-medium text-gray-700">Project Status</label>
-                    <select v-model="myProjectList.params.stage_status" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
-                        <option :value="null" disabled selected>
-                            - Select -
-                        </option>
-                        <option :value="''">
-                            All
-                        </option>
-                        <option :value="ProjectStatus.DRAFT">
-                            Draft
-                        </option>
-                        <option :value="ProjectStatus.PROPOSAL">
-                            Proposal
-                        </option>
-                        <option :value="ProjectStatus.AWARDED">
-                            Awarded
-                        </option>
-                        <option :value="ProjectStatus.BIDDING">
-                            Bidding
-                        </option>
-                        <option :value="ProjectStatus.ON_HOLD">
-                            On Hold
-                        </option>
-                    </select>
+                    <ProjectsProjectStatus v-model="myProjectList.params.stage_status" />
                 </div>
                 <div class="w-1/3 px-4">
                     <label class="block text-sm font-medium text-gray-700"> Search </label>
