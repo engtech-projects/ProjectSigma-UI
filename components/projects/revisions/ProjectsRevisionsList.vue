f503a059d79bf02afdb3007833272625c9648d83
Leonardo Empuesto <brainsnap.2012@gmail.com>
2025-07-31 14:28:05 +0800

    revisions

---
+++ components/projects/revisions/ProjectsRevisionsList.vue
@@ -0,0 +1,45 @@
+<template>
+    <LayoutBoards title="Project Revisions">
+        <div class="text-gray-500 flex flex-col gap-4 p-2">
+            <BasicSearchBar class="w-full my-2" />
+            <table class="table-auto w-full border-collapse">
+                <thead>
+                    <tr>
+                        <th
+                            class="p-2 uppercase text-left bg-gray-100 text-xs"
+                        >
+                            Project Code
+                        </th>
+                        <th
+                            class="p-2 uppercase text-left bg-gray-100 text-xs"
+                        >
+                            Project Name
+                        </th>
+                        <th
+                            class="p-2 uppercase text-left bg-gray-100 text-xs"
+                        >
+                            Version
+                        </th>
+                        <th class="p-2 uppercase text-left bg-gray-100 text-xs">
+                            Actions
+                        </th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td :colspan="4" class="text-center p-2">
+                            NO DATA
+                        </td>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
+    </LayoutBoards>
+</template>
+
+<script lang="ts" setup>
+
+</script>
+
+<style>
+</style>
